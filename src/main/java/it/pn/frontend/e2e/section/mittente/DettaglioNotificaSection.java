21f4bc55f0fc98a0255ee5af61b955d2a9e8d9c8
fmassouNTTDATA <fatimazohra.massou@emeal.nttdata.com>
2023-07-25 12:13:24 +0200

    cancel downloadFileNotifica,clickAllDownload methods  and add getLinkAvvenutaRicezione method

--- src/main/java/it/pn/frontend/e2e/section/mittente/DettaglioNotificaSection.java
+++ src/main/java/it/pn/frontend/e2e/section/mittente/DettaglioNotificaSection.java
@@ -10,13 +10,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
@@ -128,54 +125,6 @@ public class DettaglioNotificaSection extends BasePage {
         return infoNotifiche.get(i).getText();
     }
 
-    public void downloadFileNotifica(String path, String url,int nDownload) {
-        try {
-            URL urlPDF = new URL(url);
-            File pdf = new File(path+"/pdfNotificaN"+nDownload+".pdf");
-            FileUtils.copyURLToFile(urlPDF,pdf,30000,30000);
-        } catch (IOException e) {
-            logger.error("Errore nel downolad del file : "+e.getMessage());
-        }
-    }
-
-    public void download(String urlLink, File fileLoc) {
-        try {
-            byte[] buffer = new byte[1024];
-            double TotalDownload = 0.00;
-            int readbyte = 0; //Stores the number of bytes written in each iteration.
-            double percentOfDownload = 0.00;
-
-            URL url = new URL(urlLink);
-            HttpURLConnection http = (HttpURLConnection)url.openConnection();
-            double filesize = (double)http.getContentLengthLong();
-
-            BufferedInputStream input = new BufferedInputStream(http.getInputStream());
-            //fileLoc.createNewFile();
-            FileOutputStream ouputfile = new FileOutputStream(fileLoc);
-            BufferedOutputStream bufferOut = new BufferedOutputStream(ouputfile, 1024);
-            while((readbyte = input.read(buffer, 0, 1024)) >= 0) {
-                //Writing the content onto the file.
-                bufferOut.write(buffer,0,readbyte);
-                //TotalDownload is the total bytes written onto the file.
-                TotalDownload += readbyte;
-                //Calculating the percentage of download.
-                percentOfDownload = (TotalDownload*100)/filesize;
-                //Formatting the percentage up to 2 decimal points.
-                String percent = String.format("%.2f", percentOfDownload);
-                System.out.println("Downloaded "+ percent + "%");
-            }
-            System.out.println("Your download is now complete.");
-            bufferOut.close();
-            input.close();
-        }
-        catch(IOException e){
-            e.printStackTrace();
-        }
-
-    }
-
-
-
     public boolean controlloTestoFile(String path, String testoDaControllare) {
 
         File file = new File(System.getProperty("user.dir")+path);
@@ -253,7 +202,7 @@ public class DettaglioNotificaSection extends BasePage {
      this.linkAllegati.get(0).click();
     }
 
-    public void clickLinkAvvenutaRicezione() {this.linkAllegati.get(1).click();}
+    public void clickLinkAvvenutaRicezione(int i) {this.linkAllegati.get(i).click();}
 
     public void clickLinkAttestazioneOpponibile(String nomeFile) {
         try {
@@ -278,16 +227,14 @@ public class DettaglioNotificaSection extends BasePage {
         return this.elements(mailPecBy).size();
     }
 
-    public void clickAllLinkDownload() {
-        clickLinkDocumentiAllegati();
-        clickLinkAvvenutaRicezione();
-        clickLinkAttestazioneOpponibile("Attestazione opponibile a terzi: notifica presa in carico");
-    }
-
     public int getLinkAttestazioniOpponubili() {
         return attestazioniFile.size();
     }
 
+
+    public int getLinkAvvenutaRicezione() {
+        return linkAllegati.size();
+    }
     public void clickLinkAttestazionipponibile(int numeroLinkAttestazioniOpponibile) {
         attestazioniFile.get(numeroLinkAttestazioniOpponibile).click();
     }
