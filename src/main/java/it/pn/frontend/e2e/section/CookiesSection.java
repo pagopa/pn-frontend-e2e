4972e6489881af1c4fab7ff09a1215624eae79bc
fmassouNTTDATA <fatimazohra.massou@emeal.nttdata.com>
2023-08-22 10:25:37 +0200

    cancel unused statement

--- src/main/java/it/pn/frontend/e2e/section/CookiesSection.java
+++ src/main/java/it/pn/frontend/e2e/section/CookiesSection.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
