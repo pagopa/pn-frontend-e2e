2775f55ec819dcf88ceb5edb634e015762093fe3
fmassouNTTDATA <fatimazohra.massou@emeal.nttdata.com>
2023-10-19 18:05:15 +0200

    aggiornamento waitLoadLoginPageMittente() method

--- src/main/java/it/pn/frontend/e2e/pages/mittente/AcccediAreaRiservataPAPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/AcccediAreaRiservataPAPage.java
@@ -3,10 +3,7 @@ package it.pn.frontend.e2e.pages.mittente;
 
 import it.pn.frontend.e2e.common.BasePage;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
@@ -26,7 +23,7 @@ public class AcccediAreaRiservataPAPage extends BasePage {
             By titoloLabel = By.xpath("//h3[@class = 'MuiTypography-root MuiTypography-h3 css-ktw4ma']");
             this.getWebDriverWait(30).until(ExpectedConditions.visibilityOfElementLocated(titoloLabel));
             logger.info("TA_QA: Acccedi Area Riservata Page caricata");
-        }catch (TimeoutException e){
+        }catch (TimeoutException | NoSuchElementException e){
             logger.error("TA_QA: Il titolo nella pagina  Acccedi Area Riservata non è presente con errore : "+e.getMessage());
             Assert.fail("TA_QA: Il titolo nella pagina  Acccedi Area Riservata non è presente con errore : "+e.getMessage());
         }
