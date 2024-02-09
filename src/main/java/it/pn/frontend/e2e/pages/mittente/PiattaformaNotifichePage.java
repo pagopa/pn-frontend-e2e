1f64e309502206ab4f4724888aa5277f2b7f1d94
Andrea Fuso <andrea.fuso2@gmail.com>
2024-02-09 11:27:32 +0100

    test(PN-9223): add js script to scroll down to the element before clicking
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -557,6 +557,7 @@ public class PiattaformaNotifichePage extends BasePage {
         this.getWebDriverWait(10)
                 .withMessage("Il pulsante 'righe per pagina' non è presente")
                 .until(ExpectedConditions.visibilityOf(this.numeroNotificheButton));
+        this.js().executeScript("arguments[0].scrollIntoView(true);", this.numeroNotificheButton);
         this.numeroNotificheButton.click();
         this.getWebDriverWait(10)
                 .withMessage("Il pulsante '50' per assegnare il numero di notifiche per pagina non è presente")
