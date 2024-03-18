684e2d09e14e131dbb6d1bab40e079453ece53ee
Andrea Fuso <andrea.fuso2@gmail.com>
2024-03-18 11:31:17 +0100

    test: change request UtentiPGPagoPATest
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaGiuridica/UtentiPGPagoPATest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaGiuridica/UtentiPGPagoPATest.java
@@ -15,7 +15,6 @@ import java.util.Map;
 public class UtentiPGPagoPATest {
     private final Logger logger = LoggerFactory.getLogger("UtentiPGPagoPATest");
     private final WebDriver driver = Hooks.driver;
-
     private final UtentiPGPage utentiPGPage = new UtentiPGPage(this.driver);
     private Map<String, Object> datiPersonaGiuridica = new HashMap<>();
     private final DataPopulation dataPopulation = new DataPopulation();
@@ -27,12 +26,12 @@ public class UtentiPGPagoPATest {
     }
 
     @And("Si clicca sul bottone aggiungi utente")
-    public void siCliccaSulBottoneAggiungiUtente(){
+    public void siCliccaSulBottoneAggiungiUtente() {
         utentiPGPage.clickAddUserButton();
     }
 
     @And("Si visualizza correttamente la pagina aggiungi nuovo utente")
-    public void siVisualizzaCorrettamenteLaPaginaAggiungiNuovoUtente(){
+    public void siVisualizzaCorrettamenteLaPaginaAggiungiNuovoUtente() {
         utentiPGPage.waitLoadAggiungiUtentePage();
     }
 
@@ -42,49 +41,49 @@ public class UtentiPGPagoPATest {
         String name = nuovoUtente.get("name");
         String surname = nuovoUtente.get("familyName");
         String email = nuovoUtente.get("email");
-        utentiPGPage.insertData(codiceFiscale,name,surname,email);
+        utentiPGPage.insertData(codiceFiscale, name, surname, email);
     }
 
     @And("Si clicca su seleziona il prodotto {string}")
-    public void siCliccaSuSelezionaIlProdotto(String product){
+    public void siCliccaSuSelezionaIlProdotto(String product) {
         utentiPGPage.selectProduct(product);
     }
 
     @And("Si clicca su opzione amministratore e clicca bottone continua")
-    public void siCliccaSuOpzioneAmministratoreECliccaBottoneContinua(){
+    public void siCliccaSuOpzioneAmministratoreECliccaBottoneContinua() {
         utentiPGPage.selectRole();
     }
 
     @And("Si clicca sul bottone annulla nel popup")
-    public void siCliccaSulBottoneAnnullaNelPopup(){
+    public void siCliccaSulBottoneAnnullaNelPopup() {
         utentiPGPage.clickRejectPopup();
     }
 
     @And("Si clicca sul bottone continua e assegna e si visualizza messaggio di successo")
-    public void siCliccaSulBottoneContinuaEAssegna(){
+    public void siCliccaSulBottoneContinuaEAssegna() {
         utentiPGPage.clickContinueAndAssign();
         utentiPGPage.waitSuccessMessage();
     }
 
     @And("Si visualizza correttamente la pagina riepilogativa")
-    public void siVisualizzaCorrettamenteLaPaginaRiepilogativa(){
+    public void siVisualizzaCorrettamenteLaPaginaRiepilogativa() {
         utentiPGPage.waitLoadRecapPage();
     }
 
     @And("Nella pagina riepilogativa utenti si clicca sul bottone modifica")
-    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneModifica(){
+    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneModifica() {
         utentiPGPage.clickModifyButton();
         logger.info("Si verifica che campo email e conferma email sono attivi");
         utentiPGPage.checkEmailBoxActive();
     }
 
     @And("Si modifica il campo email e conferma email {string}")
-    public void siModificaIlCampoEmailEConfermaEmail(String mail){;
+    public void siModificaIlCampoEmailEConfermaEmail(String mail) {
         utentiPGPage.inserisciNuovoEmail(mail);
     }
 
     @And("Nella pagina riepilogativa utenti si clicca sul bottone conferma")
-    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneConferma(){
+    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneConferma() {
         utentiPGPage.clickConfirm();
     }
 
@@ -94,42 +93,42 @@ public class UtentiPGPagoPATest {
     }
 
     @And("Nella pagina riepilogativa utenti si clicca sul bottone rimuovi")
-    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneRimuovi(){
+    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneRimuovi() {
         utentiPGPage.clickRemoveButton();
         utentiPGPage.checkRemoveUserPopup();
     }
 
     @And("Nella pagina riepilogativa utenti si clicca sul bottone annula")
-    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneAnnula(){
+    public void nellaPaginaRiepilogativaUtentiSiCliccaSulBottoneAnnula() {
         utentiPGPage.clickRejectPopup();
     }
 
     @And("Si clicca sul bottone rimuovi dell popup")
-    public void siCliccaSulBottoneRimuoviDellPopup(){
+    public void siCliccaSulBottoneRimuoviDellPopup() {
         utentiPGPage.clickRemoveRoleButton();
         utentiPGPage.checkUserDeletedMEssage();
     }
 
     @And("Si apre dettaglio dell utente appena creato {string}")
-    public void siApreDettaglioDellUtenteAppenaCreato(String nameUtente){
+    public void siApreDettaglioDellUtenteAppenaCreato(String nameUtente) {
         String name = nameUtente.toLowerCase();
         utentiPGPage.getUserDetailsPage(name);
     }
+
     @When("Nella Pagina Notifiche persona giuridica si clicca su utenti")
     public void nellaPaginaNotifichePersonaGiuridicaSiCliccaSuUtenti() {
         utentiPGPage.clickSezioneUtenti();
-
     }
 
     @And("Nella Pagina riepilogativa si clicca su utenti")
-    public void nellaPaginaRiepilogativaSiCliccaSuUtenti(){
+    public void nellaPaginaRiepilogativaSiCliccaSuUtenti() {
         utentiPGPage.clickSezioneUtentiDaRiepilogo();
     }
 
     @And("Nella pagina utenti si effettua la login tramite credenziali")
-    public void nellaPaginaUtentiSiEffettuaLaLoginTramiteCredenziali(Map<String, String> nuovoUtente){
+    public void nellaPaginaUtentiSiEffettuaLaLoginTramiteCredenziali(Map<String, String> nuovoUtente) {
         String nome = nuovoUtente.get("user");
         String pwd = nuovoUtente.get("pwd");
-        utentiPGPage.loginUtenti(nome,pwd);
+        utentiPGPage.loginUtenti(nome, pwd);
     }
 }
