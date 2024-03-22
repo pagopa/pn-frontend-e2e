1e02fda37bfb22b17735bbdc720590b1982344bf
Gabriele Busnelli <gabriele.busnelli@cinetix-team.it>
2024-03-22 16:01:31 +0100

    ref/9163 - login helpdesk in a new tab + timeout on verification of the outage

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaGiuridica/DisserviziAppPGTest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaGiuridica/DisserviziAppPGTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
