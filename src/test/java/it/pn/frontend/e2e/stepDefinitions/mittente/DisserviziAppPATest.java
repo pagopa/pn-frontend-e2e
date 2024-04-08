c72329b760f21f7602a25bdc64ba78f31ec0417e
Andrea Fuso <andrea.fuso2@gmail.com>
2024-04-08 15:50:26 +0200

    PN-9433-e-2-e-destinatario-sezione-stato-piattaforma-disservizi-risolti (#147)
    * test(waitTime): add more time to the implicitlyWait() and loadComponentWaitTime `test-config.properties`
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test(PN-9237): update method `checkDisserviziInCorso()` less complex
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test(PN-9237): update method feature file name
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test(PN-9242): add new feature test file
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test(PN-9242): update all method and add new ones related to the new feature file test
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test: rename feature test folders
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test: change request
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test: change request
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test: change request + new enums
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test(PN-9232): add new feature test file
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test(PN-9433): add new feature test file
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test(PN-9433): add enum and update `handleService()`
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    * test: fix `getDigitalAddress()` into `RestContact`
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>
    
    ---------
    
    Signed-off-by: Andrea Fuso <andrea.fuso2@gmail.com>

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/DisserviziAppPATest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/DisserviziAppPATest.java
@@ -39,4 +39,10 @@ public class DisserviziAppPATest {
         logger.info("Si visualizza un record in elenco relativo ad un disservizio ancora in corso");
         disserviziAppPAPage.checkDisserviziInCorso();
     }
+
+    @And("Si visualizzano tutti i record in elenco relativi a disservizi risolti")
+    public void siVisualizzanoTuttiIRecordInElencoRelativiADisserviziRisolti() {
+        logger.info("Si visualizzano tutti i record in elenco relativi a disservizi risolti");
+        disserviziAppPAPage.checkDisserviziDisponibili();
+    }
 }
