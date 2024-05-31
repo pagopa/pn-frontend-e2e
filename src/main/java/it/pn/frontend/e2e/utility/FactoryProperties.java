b7edb0e9bad344dcdd232db106a5f932b3da0379
leleOFA <emanuele.didonato@pagopa.it>
2024-05-31 14:52:34 +0200

    minor fix

--- src/main/java/it/pn/frontend/e2e/utility/FactoryProperties.java
+++ src/main/java/it/pn/frontend/e2e/utility/FactoryProperties.java
@@ -1,19 +1,25 @@
 package it.pn.frontend.e2e.utility;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class FactoryProperties {
-    private String codiceFiscaleCesare = System.getProperty("codice.fiscale.cesare");
-    private String codiceFiscaleLucrezia = System.getProperty("codice.fiscale.lucrezia");
-    private String urlLoginPF = System.getProperty("url.login.pf");
-    private String urlLoginPG = System.getProperty("url.login.pg");
-    private String urlLoginPA = System.getProperty("url.login.pa");
+    private final String codiceFiscaleCesare = System.getProperty("codice.fiscale.cesare");
+    private final String codiceFiscaleLucrezia = System.getProperty("codice.fiscale.lucrezia");
+    private final String urlLoginPF = System.getProperty("url.login.pf");
+    private final String urlLoginPG = System.getProperty("url.login.pg");
+    private final String urlLoginPA = System.getProperty("url.login.pa");
 
 
-    private String tokenCesare = System.getProperty("token.login.pf.cesare");
-    private String tokenLucrezia = System.getProperty("token.login.pf.lucrezia");
+    private final String tokenCesare = System.getProperty("token.login.pf.cesare");
+    private final String tokenLucrezia = System.getProperty("token.login.pf.lucrezia");
 
-    private String tokenConvivio = System.getProperty("token.login.pg.convivio");
-    private String tokenEpistolae = System.getProperty("token.login.pg.epistolae");
+    private final String tokenConvivio = System.getProperty("token.login.pg.convivio");
+    private final String tokenEpistolae = System.getProperty("token.login.pg.epistolae");
 }
