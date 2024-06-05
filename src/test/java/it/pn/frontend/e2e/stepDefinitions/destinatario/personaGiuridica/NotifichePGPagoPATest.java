1bf83071f87b3f641a44d3e1666da0c123699988
Gabriele Busnelli <gabriele.busnelli@cinetix-team.it>
2024-06-05 18:05:02 +0200

    Unused import deleted

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaGiuridica/NotifichePGPagoPATest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaGiuridica/NotifichePGPagoPATest.java
@@ -1,6 +1,5 @@
 package it.pn.frontend.e2e.stepDefinitions.destinatario.personaGiuridica;
 
-import io.cucumber.java.bs.A;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
