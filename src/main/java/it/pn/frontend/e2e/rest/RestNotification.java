55c54865220167eb7ef06799170ff53ddb7e5711
Gabriele Busnelli <gabriele.busnelli@cinetix-team.it>
2024-06-06 14:24:37 +0200

    Unused import deleted

--- src/main/java/it/pn/frontend/e2e/rest/RestNotification.java
+++ src/main/java/it/pn/frontend/e2e/rest/RestNotification.java
@@ -5,12 +5,10 @@ import it.pn.frontend.e2e.config.CustomHttpClient;
 import it.pn.frontend.e2e.exceptions.RestNotificationException;
 import it.pn.frontend.e2e.model.NewNotificationRequest;
 import it.pn.frontend.e2e.model.NewNotificationResponse;
-import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class RestNotification {
     private static final Logger logger = LoggerFactory.getLogger("RestNotification");
