3788188b2eba7ed1f0976b43553f18204258f740
AlimaZhansultan <alima.zhansultan@netservice.eu>
2024-06-27 13:37:32 +0200

    * minor fix

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaFisica/RecapitiPersonaFisicaTest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaFisica/RecapitiPersonaFisicaTest.java
@@ -362,9 +362,9 @@ public class RecapitiPersonaFisicaTest {
     }
 
     @And("Nella pagina I Tuoi Recapiti si inserisce il codice OTP scaduto")
-    public void nellaPaginaITuoiRecapitiSiInserisceIlCodiceOTPScaduto() {
+    public void nellaPaginaITuoiRecapitiSiInserisceIlCodiceOTPScaduto() throws InterruptedException {
         logger.info("Si inserisce il codice OTP di verifica");
-        notificaMittentePagoPATest.aspettaSecondi(910);
+        driver.wait(910000);
         iTuoiRecapitiPage.sendOTP(OTP);
         recapitiDestinatarioPage.confermaButtonClickPopUp();
     }
