fda532f91c18c40564d6d853350aeaef40392083
AlimaZhansultan <alima.zhansultan@netservice.eu>
2024-07-16 15:28:23 +0200

    * Minor Fix

---
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/DashboardMittentePagoPATest.java
@@ -0,0 +1,186 @@
+package it.pn.frontend.e2e.stepDefinitions.mittente;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Splitter;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import it.pn.frontend.e2e.api.mittente.AccettazioneRichiestaNotifica;
+import it.pn.frontend.e2e.listeners.Hooks;
+import it.pn.frontend.e2e.listeners.NetWorkInfo;
+import it.pn.frontend.e2e.model.enums.AppPortal;
+import it.pn.frontend.e2e.model.singleton.NotificationSingleton;
+import it.pn.frontend.e2e.pages.mittente.AreaRiservataPAPage;
+import it.pn.frontend.e2e.pages.mittente.DashboardPage;
+import it.pn.frontend.e2e.pages.mittente.InvioNotifichePAPage;
+import it.pn.frontend.e2e.pages.mittente.PiattaformaNotifichePage;
+import it.pn.frontend.e2e.section.CookiesSection;
+import it.pn.frontend.e2e.section.mittente.*;
+import it.pn.frontend.e2e.stepDefinitions.common.BackgroundTest;
+import it.pn.frontend.e2e.stepDefinitions.destinatario.personaFisica.LoginPersonaFisicaPagoPA;
+import it.pn.frontend.e2e.stepDefinitions.destinatario.personaGiuridica.LoginPGPagoPATest;
+import it.pn.frontend.e2e.utility.CookieConfig;
+import it.pn.frontend.e2e.utility.DataPopulation;
+import it.pn.frontend.e2e.utility.WebTool;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+import static org.apache.commons.lang3.StringUtils.substring;
+
+public class DashboardMittentePagoPATest {
+
+    private static final Logger logger = LoggerFactory.getLogger("DashboardMittentePagoPATest");
+    private final WebDriver driver = Hooks.driver;
+    DashboardPage dashboardPage = new DashboardPage(this.driver);
+    PiattaformaNotifichePage piattaformaNotifichePage = new PiattaformaNotifichePage(this.driver);
+
+
+    @When("Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche")
+    public void clickSezioneStatistiche() {
+        dashboardPage.clickStatistiche();
+    }
+
+    @Then("Si visualizza correttamente la pagina di Statistiche")
+    public void siVisualizzaCorrettamenteLaPaginaStatistiche() {
+        dashboardPage.waitLoadDashboardPAPage();
+    }
+
+    @And("Nella pagina Statistiche cliccare sul bottone ultimi 6 mesi")
+    public void nellaPaginaStatisticheCliccareSulBottoneUltimi6Mesi() {
+        dashboardPage.clickUltimi6Mesi();
+    }
+
+    @And("Nella pagina Statistiche Notifiche digitali cliccare sul bottone ultimi 6 mesi")
+    public void nellaPaginaStatisticheNotificheDigitaliCliccareSulBottoneUltimi6Mesi() {
+        dashboardPage.clickUltimi6MesiNotificheDigitali();
+    }
+
+    @And("Nella pagina Statistiche si visualizza il grafico")
+    public void siVisualizzaIlGrafico() {
+        dashboardPage.checkVisualizzaGrafico();
+    }
+
+    @And("Nella pagina Statistiche si clicca il bottone esporta jpeg")
+    public void siCliccaEsportaJpeg() throws AWTException {
+        dashboardPage.clickJpegExport();
+    }
+
+    @And("Si elimina jpeg scaricato")
+    public void siEliminaJpegScaricato() {
+        dashboardPage.eliminaJpeg();
+    }
+
+    @And("Nella pagina Statistiche si visualizza il grafico Invii digitali per esito")
+    public void siVisualizzaIlGraficoInviiDigitaliPerEsito() {
+        dashboardPage.checkVisualizzaGraficoInviiDigitaliPerEsito();
+    }
+
+    @And("Nella pagina Statistiche si visualizza il grafico Tempo medio degli invii digitali")
+    public void siVisualizzaIlGraficoTempoMedioInviiDigitali() {
+        dashboardPage.checkVisualizzaGraficoTempoMedioInviiDigitali();
+    }
+
+    @And("Nella pagina Statistiche si visualizza il grafico Errori tecnici per tipologia")
+    public void siVisualizzaIlGraficoErroriTecnici() {
+        dashboardPage.checkVisualizzaGraficoErroriTecnici();
+    }
+
+    @And("Nella pagina Statistiche si visualizza il grafico Notifiche inviate per stato")
+    public void siVisualizzaIlGraficoNotificheInviatePerStato() {
+        dashboardPage.checkVisualizzaGraficoInviatePerStato();
+    }
+
+    @And("Nella pagina Statistiche si visualizza il grafico Notifiche consegnate per modalità di invio")
+    public void siVisualizzaIlGraficoNotificheConsegnatePerModalitoDiInvio() {
+        dashboardPage.checkVisualizzaGraficoConsegnatePerModalitaInvio();
+    }
+
+    @And("Nella pagina Statistiche si inserisce una data errata")
+    public void nellaPaginaStatisticheSiInserisceUnaDataErrata() {
+        dashboardPage.insertDataErrata();
+    }
+
+    @And("Nella pagina Statistiche Notifiche digitali si inserisce una data errata")
+    public void nellaPaginaStatisticheNotificheDigitaliSiInserisceUnaDataErrata() {
+        dashboardPage.insertDataErrataNotificheDigitali();
+    }
+
+    @And("Nella pagina Statistiche il bottone Filtra disabilitata")
+    public void nellaPaginaStatisticheIlBottoneFiltraDisabilitata() {
+        Assert.assertTrue("il bottone Filtra è disabilitato", piattaformaNotifichePage.verificaBottoneFiltraDisabilitato());
+    }
+
+    @And("Nella pagina Statistiche si clicca sul bottone Annulla filtri")
+    public void nellaPaginaStatisticheSiCliccaSulBottoneAnnullaFiltri() {
+        dashboardPage.clickAnnullaFiltriButton();
+    }
+
+    @And("Nella pagina Statistiche Notifiche digitali si clicca sul bottone Annulla filtri")
+    public void nellaPaginaStatisticheNotificheDigitaliSiCliccaSulBottoneAnnullaFiltri() {
+        dashboardPage.clickAnnullaFiltriButtonNotificheDigitali();
+    }
+
+    @And("Nella pagina Statistiche si clicca sul bottone Filtra")
+    public void nellaPaginaStatisticheSiCliccaSulBottoneFiltra() {
+        dashboardPage.clickFiltraButton();
+    }
+
+    @And("Nella pagina Statistiche Notifiche digitali si clicca sul bottone Filtra")
+    public void nellaPaginaStatisticheNotifichDigitaliSiCliccaSulBottoneFiltra() {
+        dashboardPage.clickFiltraButtonNotificheDigitali();
+    }
+
+    @And("Nella pagina Statistiche si inserisce una data corretta")
+    public void nellaPaginaStatisticheSiInserisceUnaDataCorretta() {
+        dashboardPage.insertDataCorretta();
+    }
+
+    @And("Nella pagina Statistiche Notifiche digitali si inserisce una data corretta")
+    public void nellaPaginaStatisticheNotificheDigitaliSiInserisceUnaDataCorretta() {
+        dashboardPage.insertDataCorrettaNotificheDigitali();
+    }
+
+    @And("Si verifica che tipo di grafico è {string}")
+    public void siVerificaCheTipoDiGraficoE(String tipoGrafico) {
+        dashboardPage.checkTipoGrafico(tipoGrafico);
+    }
+
+    @And("Si verifica che tipo di grafico per modalita di invio è {string}")
+    public void siVerificaCheTipoDiGraficoPerModalitaInvio(String tipoGrafico) {
+        dashboardPage.checkTipoGraficoPerModalitaInvio(tipoGrafico);
+    }
+
+    @And("Si cambia tipo di grafico")
+    public void siCambiaTipoDiGrafico() {
+        dashboardPage.cambiaTipoGrafico();
+    }
+
+    @And("Si cambia tipo di grafico per modalita di invio")
+    public void siCambiaTipoDiGraficoPerModalitaInvio() {
+        dashboardPage.cambiaTipoGraficoPerModalitaInvio();
+    }
+
+    @And("Si sceglie opzione Settimane")
+    public void siSceglieOpzioneSettimane() {
+        dashboardPage.sceglieOpzioneSettimane();
+    }
+
+    @And("Si sceglie opzione Giorni")
+    public void siSceglieOpzioneGiorni() {
+        dashboardPage.sceglieOpzioneGiorni();
+    }
+
+
+}
---
+++ src/test/resources/feature/2-mittente/dashboard_mittenti.feature
@@ -0,0 +1,133 @@
+Feature: Mittente invia una notifica digitale al destinatario con indirizzo fornito dalla PA
+
+  @TestSuite
+  @DashboardMittenti
+
+  @12036
+  Scenario: PN-12036
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    Then Si visualizza correttamente la pagina di Statistiche
+    And Logout da portale mittente
+
+
+  @12037
+  Scenario: PN-12037
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche cliccare sul bottone ultimi 6 mesi
+    And Nella pagina Statistiche si visualizza il grafico
+    And Nella pagina Statistiche si clicca il bottone esporta jpeg
+    And Si elimina jpeg scaricato
+    And Logout da portale mittente
+
+
+  @12039
+  Scenario: PN-12039
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche cliccare sul bottone ultimi 6 mesi
+    And Nella pagina Statistiche si visualizza il grafico
+    And Nella pagina Statistiche si inserisce una data errata
+    And Nella pagina Statistiche il bottone Filtra disabilitata
+    And Nella pagina Statistiche si inserisce una data corretta
+    And Nella pagina Statistiche si clicca sul bottone Filtra
+    And Nella pagina Statistiche si visualizza il grafico
+    Then Nella pagina Statistiche si clicca sul bottone Annulla filtri
+    And Logout da portale mittente
+
+
+  @12040
+  Scenario: PN-12040
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche cliccare sul bottone ultimi 6 mesi
+    And Nella pagina Statistiche si visualizza il grafico
+    And Si verifica che tipo di grafico è "Aggregato"
+    And Si cambia tipo di grafico
+    And Nella pagina Statistiche si visualizza il grafico
+    And Si sceglie opzione Giorni
+    Then Nella pagina Statistiche si visualizza il grafico
+    And Logout da portale mittente
+
+
+  @12041
+  Scenario: PN-12041
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche cliccare sul bottone ultimi 6 mesi
+    Then Nella pagina Statistiche si visualizza il grafico Notifiche inviate per stato
+    And Logout da portale mittente
+
+
+  @12042
+  Scenario: PN-12042
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche cliccare sul bottone ultimi 6 mesi
+    And Nella pagina Statistiche si visualizza il grafico Notifiche consegnate per modalità di invio
+    And Si verifica che tipo di grafico per modalita di invio è "Aggregato"
+    And Si cambia tipo di grafico per modalita di invio
+    And Si sceglie opzione Giorni
+    Then Nella pagina Statistiche si visualizza il grafico Notifiche consegnate per modalità di invio
+    And Logout da portale mittente
+
+
+  @12043
+  Scenario: PN-12043
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche Notifiche digitali cliccare sul bottone ultimi 6 mesi
+    And Nella pagina Statistiche si visualizza il grafico Invii digitali per esito
+    And Nella pagina Statistiche Notifiche digitali si inserisce una data errata
+    And Nella pagina Statistiche il bottone Filtra disabilitata
+    And Nella pagina Statistiche Notifiche digitali si inserisce una data corretta
+    And Nella pagina Statistiche Notifiche digitali si clicca sul bottone Filtra
+    And Nella pagina Statistiche si visualizza il grafico Invii digitali per esito
+    Then Nella pagina Statistiche Notifiche digitali si clicca sul bottone Annulla filtri
+    And Logout da portale mittente
+
+
+  @12044
+  Scenario: PN-12044
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche Notifiche digitali cliccare sul bottone ultimi 6 mesi
+    Then Nella pagina Statistiche si visualizza il grafico Invii digitali per esito
+    And Logout da portale mittente
+
+  @12045
+  Scenario: PN-12045
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche Notifiche digitali cliccare sul bottone ultimi 6 mesi
+    Then Nella pagina Statistiche si visualizza il grafico Tempo medio degli invii digitali
+    And Logout da portale mittente
+
+
+  @12046
+  Scenario: PN-12046
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Si visualizza correttamente la pagina di Statistiche
+    And Nella pagina Statistiche Notifiche digitali cliccare sul bottone ultimi 6 mesi
+    Then Nella pagina Statistiche si visualizza il grafico Errori tecnici per tipologia
+    And Logout da portale mittente
+
+
+
+  @12047
+  Scenario: PN-12047
+    Given PA - Si effettua la login tramite token exchange, e viene visualizzata la dashboard
+    When Nella pagina Piattaforma Notifiche cliccare sul bottone Statistiche
+    And Nella pagina Statistiche cliccare sul bottone ultimi 6 mesi
+    Then Nella pagina Statistiche si visualizza il grafico
+    And Logout da portale mittente
