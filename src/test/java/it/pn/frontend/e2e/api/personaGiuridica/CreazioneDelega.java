220034dd1d0ec4edcbfe578263a657d9d2107585
AlimaZhansultan <alima.zhansultan@netservice.eu>
2024-09-03 08:12:01 +0200

    * eliminated not used methods and variables

--- src/test/java/it/pn/frontend/e2e/api/personaGiuridica/CreazioneDelega.java
+++ src/test/java/it/pn/frontend/e2e/api/personaGiuridica/CreazioneDelega.java
@@ -8,16 +8,10 @@ import org.slf4j.LoggerFactory;
 public class CreazioneDelega {
     private static final Logger logger = LoggerFactory.getLogger("CreazioneDelega");
     private String url;
-    private String body;
-    private String authorizationToken;
     private String responseBody;
 
     public String getResponseBody() {
         return responseBody;
-    }
-
-    public void setResponseBody(String responseBody) {
-        this.responseBody = responseBody;
     }
 
 
