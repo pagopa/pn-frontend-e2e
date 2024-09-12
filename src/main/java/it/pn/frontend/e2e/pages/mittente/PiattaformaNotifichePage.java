0cf93f304060d2a67179e0b2b51229e922d3d9b4
angelo.minisci <angelo.minisci@grupposcai.it>
2024-09-12 18:11:52 +0200

    fix

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -213,7 +213,7 @@ public class PiattaformaNotifichePage extends BasePage {
     }
 
     public void inserimentoArcoTemporale(String da, String a) {
-        WebTool.waitTime(10);
+        WebTool.waitTime(20);
         dataInizioField = driver.findElement(By.id("startDate"));
         dataFineField = driver.findElement(By.id("endDate"));
 
