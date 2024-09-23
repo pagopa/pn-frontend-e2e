f0d2685dd1425b5e58a3a63a7a4462fdef80bb81
angelo.minisci <angelo.minisci@grupposcai.it>
2024-09-23 21:47:48 +0200

    fix

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -284,7 +284,8 @@ public class PiattaformaNotifichePage extends BasePage {
 
         // Step 4: Select a date (e.g., the 15th day of the current month)
         WebTool.waitTime(5);
-        WebElement dateToSelect = calendar.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//button[contains(text(), '" + dayDa + "')]"));
+        WebElement dateToSelect = calendar.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//div[contains(@class,'MuiDayCalendar-monthContainer')]//*[text()='" + dayDa + "']"));
+
         dateToSelect = getWebDriverWait(10).until(ExpectedConditions.elementToBeClickable(dateToSelect));
         dateToSelect.click();
 
@@ -340,7 +341,7 @@ public class PiattaformaNotifichePage extends BasePage {
 
 
         // Step 4: Select a date (e.g., the 15th day of the current month)
-        WebElement dateToSelect1 = calendar1.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//button[contains(text(), '" + dayA + "')]"));
+        WebElement dateToSelect1 = calendar1.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//div[contains(@class,'MuiDayCalendar-monthContainer')]//*[text()='" + dayA + "']"));
         dateToSelect1 = getWebDriverWait(10).until(ExpectedConditions.elementToBeClickable(dateToSelect1));
         dateToSelect1.click();
 
