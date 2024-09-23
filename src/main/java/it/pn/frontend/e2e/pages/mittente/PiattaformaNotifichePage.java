9e74110bd97a790284bef310e02be2c7fa7ccb56
angelo.minisci <angelo.minisci@grupposcai.it>
2024-09-23 20:15:41 +0200

    Refactor

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -320,7 +320,7 @@ public class PiattaformaNotifichePage extends BasePage {
         //  monthDaToSelect.click();
         // Step 4: Select a date (e.g., the 15th day of the current month)
         WebTool.waitTime(5);
-        WebElement dateToSelect = calendar.findElement(By.xpath("//button[contains(text(), '" + dayDa + "')]"));
+        WebElement dateToSelect = calendar.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//button[contains(text(), '" + dayDa + "')]"));
         dateToSelect.click();
 
 
@@ -413,7 +413,7 @@ public class PiattaformaNotifichePage extends BasePage {
 
 
         // Step 4: Select a date (e.g., the 15th day of the current month)
-        WebElement dateToSelect1 = calendar1.findElement(By.xpath("//button[contains(text(), '" + dayA + "')]"));
+        WebElement dateToSelect1 = calendar1.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//button[contains(text(), '" + dayA + "')]"));
         dateToSelect1.click();
 
         // js().executeScript("arguments[0].value='19/09/2024';", dataInizioFieldList.get(3));
