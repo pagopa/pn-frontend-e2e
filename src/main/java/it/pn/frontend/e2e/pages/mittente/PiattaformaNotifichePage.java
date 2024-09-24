d954feb71fb8308f6fdc973fb1a8008fa1abb4c1
angelo.minisci <angelo.minisci@grupposcai.it>
2024-09-24 12:55:03 +0200

    Restore

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -285,7 +285,7 @@ public class PiattaformaNotifichePage extends BasePage {
 
         // Step 4: Select a date (e.g., the 15th day of the current month)
         WebTool.waitTime(5);
-        WebElement dateToSelect = monthDaToSelect.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//div[contains(@class,'MuiDayCalendar-monthContainer')]//*[text()='" + dayDa + "']"));
+        WebElement dateToSelect = calendar.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//div[contains(@class,'MuiDayCalendar-monthContainer')]//*[text()='" + dayDa + "']"));
         dateToSelect = getWebDriverWait(10).until(ExpectedConditions.elementToBeClickable(dateToSelect));
         //js().executeScript("arguments[0].click()", dateToSelect);
         dateToSelect.click();
@@ -344,8 +344,8 @@ public class PiattaformaNotifichePage extends BasePage {
 
 
         // Step 4: Select a date (e.g., the 15th day of the current month)
-       // Adjust based on your app
-        WebElement dateToSelect1 = monthAToSelect.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//div[contains(@class,'MuiDayCalendar-monthContainer')]//*[text()='" + dayA + "']"));
+        // Adjust based on your app
+        WebElement dateToSelect1 = calendar1.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//div[contains(@class,'MuiDayCalendar-monthContainer')]//*[text()='" + dayA + "']"));
         dateToSelect1 = getWebDriverWait(10).until(ExpectedConditions.elementToBeClickable(dateToSelect1));
         //js().executeScript("arguments[0].click()", dateToSelect1);
         dateToSelect1.click();
