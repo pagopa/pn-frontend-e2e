7eb5e3d158bb615961552c3dea1a3c17f85e5140
scritelli <stefano.critelli@alten.it>
2024-10-15 10:15:02 +0200

    Rilascio fix e prova di git

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/ApiKeysTest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/ApiKeysTest.java
@@ -34,6 +34,7 @@ public class ApiKeysTest {
 
     }
 
+
     @And("Si visualizza correttamente la pagina Api Key")
     public void siVisualizzaCorrettamenteLaPaginaApiKey() {
         logger.info("Si visualizza correttamente la pagina Api Key");
