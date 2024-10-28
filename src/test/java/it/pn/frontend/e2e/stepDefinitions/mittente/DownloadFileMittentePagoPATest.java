1122830a46b82783a8243dab655e3771df44bdab
scritelli <stefano.critelli@alten.it>
2024-10-28 13:13:15 +0100

    Ristrutturazione classe con springboot in attesa di test con la suite di mittente

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/DownloadFileMittentePagoPATest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/DownloadFileMittentePagoPATest.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import it.pn.frontend.e2e.common.DettaglioNotificaSection;
 import it.pn.frontend.e2e.config.WebDriverConfig;
-import it.pn.frontend.e2e.listeners.Hooks;
 import it.pn.frontend.e2e.pages.mittente.DisserviziAppPAPage;
 import it.pn.frontend.e2e.pages.mittente.PiattaformaNotifichePage;
 import it.pn.frontend.e2e.section.mittente.DettaglioNotificaMittenteSection;
@@ -13,12 +12,12 @@ import it.pn.frontend.e2e.stepDefinitions.common.BackgroundTest;
 import it.pn.frontend.e2e.utility.DataPopulation;
 import it.pn.frontend.e2e.utility.DownloadFile;
 import it.pn.frontend.e2e.utility.WebTool;
-
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.util.HashMap;
@@ -26,20 +25,27 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-
+@Component
 public class DownloadFileMittentePagoPATest {
+
     private static final Logger logger = LoggerFactory.getLogger("DownloadFileMittentePagoPATest");
-    private final WebDriver driver = Hooks.driver;
-    private Map<String, Object> datiNotifica = new HashMap<>();
-    private DownloadFile downloadFile;
+
+    @Autowired
+    private WebDriver driver;
 
     @Autowired
     private WebDriverConfig webDriverConfig;
+
+    @Autowired
+    private DownloadFile downloadFile;
+
+    private Map<String, Object> datiNotifica = new HashMap<>();
 
     @When("Nella pagina Piattaforma Notifiche si clicca sulla notifica restituita")
     public void clickNotificaRestituita() {
         logger.info("Si clicca sulla notifica restituita");
         logger.info("GENERATED IUN: {}", System.getProperty("IUN"));
+
         PiattaformaNotifichePage piattaformaNotifichePage = new PiattaformaNotifichePage(driver);
         piattaformaNotifichePage.selezionaNotificaIUN(System.getProperty("IUN"));
     }
@@ -48,7 +54,7 @@ public class DownloadFileMittentePagoPATest {
     public void siVisualizzaCorrettamenteLaSezioneDettaglioNotifica() {
         logger.info("Viene caricato correttamente la sezione Dettaglio Notifica");
 
-        DettaglioNotificaMittenteSection dettaglioNotificaMittenteSection = new DettaglioNotificaMittenteSection(this.driver);
+        DettaglioNotificaMittenteSection dettaglioNotificaMittenteSection = new DettaglioNotificaMittenteSection(driver);
         dettaglioNotificaMittenteSection.waitLoadDettaglioNotificaSection();
     }
 
@@ -56,17 +62,18 @@ public class DownloadFileMittentePagoPATest {
     public void downloadECheckFile() {
         logger.info("Si scaricano tutti i file all'interno della notifica");
 
-        DettaglioNotificaMittenteSection dettaglioNotificaMittenteSection = new DettaglioNotificaMittenteSection(this.driver);
-        DettaglioNotificaSection dettaglioNotificaSection = new DettaglioNotificaSection(this.driver);
+        DettaglioNotificaMittenteSection dettaglioNotificaMittenteSection = new DettaglioNotificaMittenteSection(driver);
+        DettaglioNotificaSection dettaglioNotificaSection = new DettaglioNotificaSection(driver);
         DataPopulation dataPopulation = new DataPopulation();
 
         String workingDirectory = System.getProperty("user.dir");
         File pathCartella = new File(workingDirectory + "/src/test/resources/dataPopulation/downloadFileNotifica/mittente");
-        downloadFile = new DownloadFile(this.driver);
+
         boolean headless = webDriverConfig.getHeadless().equalsIgnoreCase("true");
         if (!downloadFile.controlloEsistenzaCartella(pathCartella)) {
             pathCartella.mkdirs();
         }
+
         this.datiNotifica = dataPopulation.readDataPopulation("datiNotifica.yaml");
         int count = 1;
 
@@ -78,40 +85,33 @@ public class DownloadFileMittentePagoPATest {
         } else {
             codiceIUN = dettaglioNotificaMittenteSection.getInfoNotifica(4);
         }
-        dettaglioNotificaMittenteSection.clickLinkDocumentiAllegati();
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
+        dettaglioNotificaMittenteSection.clickLinkDocumentiAllegati();
+        WebTool.waitTime(5);
 
         final String filepath = workingDirectory + "/src/test/resources/dataPopulation/downloadFileNotifica/mittente/notificaN";
-
         final String urlDocumenti = webDriverConfig.getBaseUrl() + "notifications/sent/" + codiceIUN + "/documents/";
-
         final String urlDocumentiAllegati = downloadFile.getUrl(urlDocumenti);
         File file = new File(filepath + count + ".pdf");
+
         if (headless && urlDocumentiAllegati.isEmpty()) {
             String testoLink = dettaglioNotificaMittenteSection.getTextDocumentiAllegati();
             logger.error("Non è stato recuperato url per il download per il link: " + testoLink);
             Assertions.fail("Non è stato recuperato url per il download per il link: " + testoLink);
         }
+
         downloadFile.download(urlDocumentiAllegati, file, headless);
         if (!headless) {
             dettaglioNotificaSection.goBack();
         }
+
         dettaglioNotificaSection.waitLoadDettaglioNotificaDESection();
-        count = count + 1;
+        count++;
 
         int numeroLinkAvvenutaRicezione = dettaglioNotificaMittenteSection.getLinkAvvenutaRicezione();
         for (int i = 1; i < numeroLinkAvvenutaRicezione; i++) {
             dettaglioNotificaMittenteSection.clickLinkAvvenutaRicezione(i);
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            WebTool.waitTime(5);
             final String url = webDriverConfig.getBaseUrl() + "notifications/sent/" + codiceIUN + "/documents/AAR?documentId=safestorage:";
             final String urlAvvenutaRicezione = downloadFile.getUrl(url);
             if (headless && urlAvvenutaRicezione.isEmpty()) {
@@ -120,55 +120,52 @@ public class DownloadFileMittentePagoPATest {
                 Assertions.fail("Non è stato recuperato url per il download per il link: " + testoLink);
             }
             file = new File(filepath + count + ".pdf");
-            count = count + 1;
-
             downloadFile.download(urlAvvenutaRicezione, file, headless);
             if (!headless) {
-                dettaglioNotificaSection.goBack();
+                dettaglioNotificaMittenteSection.goBack();
             }
             dettaglioNotificaSection.waitLoadDettaglioNotificaDESection();
+            count++;
         }
 
         int numeroLinkAttestazioniOpponibile = dettaglioNotificaSection.getLinkAttestazioniOpponibili();
-
         for (int i = 0; i < numeroLinkAttestazioniOpponibile; i++) {
             dettaglioNotificaSection.clickLinkAttestazioniOpponibile(i);
-            try {
-                TimeUnit.SECONDS.sleep(5);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            final String urlFileAttestazioneOppponibile = downloadFile.getUrl(webDriverConfig.getBaseUrl());
-            if (headless && urlFileAttestazioneOppponibile.isEmpty()) {
+            WebTool.waitTime(5);
+            final String urlFileAttestazioneOpponibile = downloadFile.getUrl(webDriverConfig.getBaseUrl());
+            if (headless && urlFileAttestazioneOpponibile.isEmpty()) {
                 String testoLink = dettaglioNotificaSection.getTextLinkAttestazioniOpponibili(i);
                 logger.error("Non è stato recuperato url per il download per il link: " + testoLink);
                 Assertions.fail("Non è stato recuperato url per il download per il link: " + testoLink);
             }
             file = new File(filepath + count + ".pdf");
-            count = count + 1;
-            downloadFile.download(urlFileAttestazioneOppponibile, file, headless);
+            downloadFile.download(urlFileAttestazioneOpponibile, file, headless);
             if (!headless) {
                 dettaglioNotificaSection.goBack();
             }
             dettaglioNotificaSection.waitLoadDettaglioNotificaDESection();
+            count++;
         }
-        count = count - 1;
         final String pathOfDownloadedFile = workingDirectory + "/src/test/resources/dataPopulation/downloadFileNotifica/mittente";
-        downloadFile.controlloDownload(pathOfDownloadedFile, count);
+        downloadFile.controlloDownload(pathOfDownloadedFile, count - 1);
+    }
 
+    @And("Si visualizza correttamente la tabella dei disservizi")
+    public void siVisualizzaCorrettamenteLaTabellaDeiDisservizi() {
+        logger.info("Si visualizza correttamente la tabella dei disservizi");
+        DisserviziAppPAPage disserviziAppPAPage = new DisserviziAppPAPage(driver);
+        disserviziAppPAPage.waitLoadDisserviziTable();
     }
 
     @And("Nella sezione Dettaglio Notifiche si scarica il documento allegato")
     public void downloadDocumentiAllegati() {
         logger.info("Si scaricano solo i documenti Allegati all'interno della notifica");
 
-        DettaglioNotificaMittenteSection dettaglioNotificaMittenteSection = new DettaglioNotificaMittenteSection(this.driver);
-        DettaglioNotificaSection dettaglioNotificaSection = new DettaglioNotificaSection(this.driver);
-        DataPopulation dataPopulation = new DataPopulation();
+        DettaglioNotificaMittenteSection dettaglioNotificaMittenteSection = new DettaglioNotificaMittenteSection(driver);
+        DettaglioNotificaSection dettaglioNotificaSection = new DettaglioNotificaSection(driver);
 
         String workingDirectory = System.getProperty("user.dir");
         File pathCartella = new File(workingDirectory + "/src/test/resources/dataPopulation/downloadFileNotifica/mittente");
-        DownloadFile downloadFile = new DownloadFile(this.driver);
         boolean headless = webDriverConfig.getHeadless().equalsIgnoreCase("true");
         if (!downloadFile.controlloEsistenzaCartella(pathCartella)) {
             pathCartella.mkdirs();
@@ -184,11 +181,7 @@ public class DownloadFileMittentePagoPATest {
             codiceIUN = dettaglioNotificaMittenteSection.getInfoNotifica(4);
         }
         dettaglioNotificaMittenteSection.clickLinkDocumentiAllegati();
-        try {
-            TimeUnit.SECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        WebTool.waitTime(5);
 
         final String filepath = workingDirectory + "/src/test/resources/dataPopulation/downloadFileNotifica/mittente/notificaN";
         final String urlDocumenti = webDriverConfig.getBaseUrl() + "notifications/sent/" + codiceIUN + "/attachments/documents/0";
@@ -204,11 +197,11 @@ public class DownloadFileMittentePagoPATest {
             dettaglioNotificaMittenteSection.goBack();
         }
         dettaglioNotificaSection.waitLoadDettaglioNotificaDESection();
-        count++;
-
         final String pathOfDownloadedFile = workingDirectory + "/src/test/resources/dataPopulation/downloadFileNotifica/mittente";
         downloadFile.controlloDownload(pathOfDownloadedFile, count);
     }
+
+
 
     @Then("Si clicca sul documento allegato")
     public void clickDocumentoAllegato() {
