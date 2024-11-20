059c09835e28decc14996c4ac2df3639eda7c647
angelo.minisci <angelo.minisci@grupposcai.it>
2024-11-20 12:06:48 +0100

    add eng lang

--- src/main/java/it/pn/frontend/e2e/pages/destinatario/personaFisica/NotifichePFPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/destinatario/personaFisica/NotifichePFPage.java
@@ -20,7 +20,7 @@ public class NotifichePFPage extends BasePage {
     private static final Logger logger = LoggerFactory.getLogger(NotifichePFPage.class);
 
 
-    @FindBy(id = "Le tue notifiche-page")
+    @FindBy(id = "Your notifications-page")
     private WebElement titleLabel;
 
     @FindBy(id = "notifications-table")
@@ -80,7 +80,7 @@ public class NotifichePFPage extends BasePage {
     }
 
     public void waitLoadNotificheDEPage() {
-        titleLabel = driver.findElement(By.id("Le tue notifiche-page"));
+        titleLabel = driver.findElement(By.id("Your notifications-page"));
         tableNotifiche = driver.findElement(By.id("notifications-table"));
         getWebDriverWait(10).withMessage("Notifiche DE Page non caricata correttamente: il titolo non è visibile").until(ExpectedConditions.visibilityOf(titleLabel));
         getWebDriverWait(10).withMessage("Notifiche DE Page non caricata correttamente: la tabella delle notifiche non è visibile").until(ExpectedConditions.visibilityOf(tableNotifiche));
