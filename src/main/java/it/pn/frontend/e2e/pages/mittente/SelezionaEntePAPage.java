55fdf62412f0cfa51c2067c129fa40791f36ed05
angelo.minisci <angelo.minisci@grupposcai.it>
2024-11-22 15:10:45 +0100

    add  log

--- src/main/java/it/pn/frontend/e2e/pages/mittente/SelezionaEntePAPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/SelezionaEntePAPage.java
@@ -47,6 +47,7 @@ public class SelezionaEntePAPage extends BasePage {
     public void waitLoadSelezionaEntePAPage() {
         try {
             webTool.waitTime(25);
+            logger.info("HTML..."+driver.getPageSource());
             WebElement titlePage = driver.findElement(By.xpath("//h3[text()='Seleziona il tuo ente']"));
             WebElement searchField = driver.findElement(By.id("search"));
             getWebDriverWait(30)
