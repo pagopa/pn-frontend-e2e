6cb01d9990f1831b07c8815ff8e52ba2ee3f6518
angelo.minisci <angelo.minisci@grupposcai.it>
2024-11-25 16:35:11 +0100

    refactor

--- src/main/java/it/pn/frontend/e2e/section/destinatario/personaFisica/HeaderPFSection.java
+++ src/main/java/it/pn/frontend/e2e/section/destinatario/personaFisica/HeaderPFSection.java
@@ -43,11 +43,11 @@ public class HeaderPFSection extends BasePage {
     public void waitLoadHeaderDESection() {
         try {
             webTool.waitTime(15);
-            titleLabel = driver.findElement(By.xpath("//a[@title='Sito di PagoPA S.p.A.']"));
-            webTool.waitTime(15);
-            buttonProfile = driver.findElement(By.xpath("//button[@aria-label='party-menu-button']"));
-            getWebDriverWait(30).withMessage("il titolo del header non è visibile").until(ExpectedConditions.visibilityOf(titleLabel));
-            getWebDriverWait(30).withMessage("menu dell'utente non è visibile").until(ExpectedConditions.visibilityOf(buttonProfile));
+            //titleLabel = driver.findElement(By.xpath("//a[@title='Sito di PagoPA S.p.A.']"));
+           // webTool.waitTime(15);
+           // buttonProfile = driver.findElement(By.xpath("//button[@aria-label='party-menu-button']"));
+            getWebDriverWait(30).withMessage("il titolo del header non è visibile").until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//a[@title='Sito di PagoPA S.p.A.']"))));
+            getWebDriverWait(30).withMessage("menu dell'utente non è visibile").until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//button[@aria-label='party-menu-button']"))));
             logger.info("Header DE Section caricata");
         } catch (TimeoutException e) {
             logger.error("Header DE Section non caricata con errore : " + e.getMessage());
