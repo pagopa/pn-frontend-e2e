47a37784496176b45a119f26df1e198918d41375
angelo.minisci <angelo.minisci@grupposcai.it>
2024-11-25 14:53:22 +0100

    add wait

--- src/main/java/it/pn/frontend/e2e/pages/mittente/SelezionaEntePAPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/SelezionaEntePAPage.java
@@ -46,8 +46,8 @@ public class SelezionaEntePAPage extends BasePage {
 
     public void waitLoadSelezionaEntePAPage() {
         try {
-            webTool.waitTime(20);
-            WebElement titlePage = driver.findElement(By.xpath("//h3[text()='Seleziona il tuo ente']"));
+            webTool.waitTime(25);
+            WebElement titlePage   = driver.findElement(By.xpath("//h3[text()='Seleziona il tuo ente']"));
             WebElement searchField = driver.findElement(By.id("search"));
             getWebDriverWait(30)
                     .withMessage("Titolo 'Seleziona il tuo ente' della pagina non è visibile")
