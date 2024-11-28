88fdf921474d8ed07f456a868ed24d26143a0d14
scritelli <stefano.critelli@alten.it>
2024-11-28 17:09:27 +0100

    risolto by x path in selezionareContinuaButton

--- src/main/java/it/pn/frontend/e2e/pages/mittente/InvioNotifichePAPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/InvioNotifichePAPage.java
@@ -26,8 +26,9 @@ public class InvioNotifichePAPage extends BasePage {
 
     public void selezionareContinuaButton() {
         webTool.waitTime(5);
+        //continuaButton = driver.findElement(By.id("step-submit"));
+        getWebDriverWait(10).withMessage("Il bottone continua nella pagina invio notifica PA non è cliccabile").until(ExpectedConditions.elementToBeClickable(By.id("step-submit")));
         continuaButton = driver.findElement(By.id("step-submit"));
-        getWebDriverWait(10).withMessage("Il bottone continua nella pagina invio notifica PA non è cliccabile").until(ExpectedConditions.elementToBeClickable(this.continuaButton));
         scrollToElementAndClick(this.continuaButton);
     }
 }
