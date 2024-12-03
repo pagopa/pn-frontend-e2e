78372b5b4711ed5df1c6b887d8b64e45af3cf7a8
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-03 07:42:58 +0100

    fix

--- src/main/java/it/pn/frontend/e2e/section/destinatario/personaFisica/LeTueDelegheSection.java
+++ src/main/java/it/pn/frontend/e2e/section/destinatario/personaFisica/LeTueDelegheSection.java
@@ -216,7 +216,7 @@ public class LeTueDelegheSection extends BasePage {
     }
 
     public void clickOpzioneAccetta() {
-        webTool.waitTime(1);
+        webTool.waitTime(5);
         WebElement acceptDelegaButton = driver.findElement(By.xpath("//*[@id='accept-button']"));
         acceptDelegaButton.click();
     }
