db064baa686d8cea28b6a970bda5cd283b8bdb2f
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-03 10:10:11 +0100

    fix singleton

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/NotificaMittentePagoPATest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/NotificaMittentePagoPATest.java
@@ -116,7 +116,7 @@ public class NotificaMittentePagoPATest  extends BasePage {
     @Autowired
     private RestNotification restNotification;
 
-
+ 
     @PostConstruct
     public void init(){
         logger.info("INIT TEST...: ");
@@ -1375,9 +1375,9 @@ public class NotificaMittentePagoPATest  extends BasePage {
     public void siSelezionaLaNotifica() {
         piattaformaNotifichePage.setNotificationSingletonParam(notificationSingleton);
         backgroundTest.setPiattaformaNotifichePage(piattaformaNotifichePage);
-        String iun = notificationSingleton.getIun(HooksNew.scenario);
+       // String iun = notificationSingleton.getIun(HooksNew.scenario);
         logger.info("IUN............."+ backgroundTest.getPiattaformaNotifichePage().getNotificationSingletonParam().getIun(HooksNew.scenario));
-        iun = backgroundTest.getPiattaformaNotifichePage().getNotificationSingletonParam().getIun(HooksNew.scenario);
+        String iun = backgroundTest.getPiattaformaNotifichePage().getNotificationSingletonParam().getIun(HooksNew.scenario);
         backgroundTest.siFiltraLaTabellaDelleNotifichePerIUNDestinatario(iun);
     }
 
