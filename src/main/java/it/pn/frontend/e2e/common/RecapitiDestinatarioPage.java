95eee46103e59fe63587efba3f7e24364a282edf
scritelli <stefano.critelli@alten.it>
2024-12-03 15:57:35 +0100

    Aggiornato verificaMailAssociata per recapiti

--- src/main/java/it/pn/frontend/e2e/common/RecapitiDestinatarioPage.java
+++ src/main/java/it/pn/frontend/e2e/common/RecapitiDestinatarioPage.java
@@ -306,8 +306,8 @@ public class RecapitiDestinatarioPage extends BasePage {
 
     public boolean verificaMailAssociata() {
         try {
-            WebElement emailAssociata = driver.findElement(By.id("default_email-typography"));
-            getWebDriverWait(10).withMessage("L'email di cortesia non è presente").until(ExpectedConditions.visibilityOf(emailAssociata));
+            getWebDriverWait(10).withMessage("L'email di cortesia non è presente").until(ExpectedConditions.visibilityOfElementLocated(By.id("default_email-typography")));
+            // WebElement emailAssociata = driver.findElement(By.id("default_email-typography"));
             //getWebDriverWait(10).withMessage("L'email di cortesia non è presente").until(ExpectedConditions.visibilityOfElementLocated(By.id("default_email-typography")));
             return true;
         } catch (NoSuchElementException | TimeoutException e) {
