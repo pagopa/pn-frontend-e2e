181118fc82f8b80ed9307e75edb5da6fec26012a
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-04 15:49:54 +0100

    add wait

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -221,10 +221,10 @@ public class PiattaformaNotifichePage extends BasePage {
     public boolean verificaEsistenzaEPassaggioPagina() {
         js().executeScript("window.scrollBy(0,document.body.scrollHeight)");
         try {
-            secondPage = driver.findElement(By.id("page2"));
-            getWebDriverWait(10).withMessage("Il bottone pagina 2 non è visibile").until(ExpectedConditions.visibilityOf(secondPage));
+           // secondPage = driver.findElement(By.id("page2"));
+            getWebDriverWait(10).withMessage("Il bottone pagina 2 non è visibile").until(ExpectedConditions.visibilityOf(driver.findElement(By.id("page2"))));
             logger.info("Bottone pagina 2 trovato");
-            secondPage.click();
+            driver.findElement(By.id("page2")).click();
             return true;
         } catch (TimeoutException e) {
             logger.error("bottone pagina 2 non trovata con errore: " + e.getMessage());
