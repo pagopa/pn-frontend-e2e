873d89160b18895919934afcdf0f2c79e332c801
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-06 18:52:14 +0100

    refactor

--- src/main/java/it/pn/frontend/e2e/pages/destinatario/personaGiuridica/AccediAreaRiservataPGPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/destinatario/personaGiuridica/AccediAreaRiservataPGPage.java
@@ -31,7 +31,7 @@ public class AccediAreaRiservataPGPage extends BasePage {
            // spidButton = driver.findElement(By.id("spidButton"));
             By titleBy = By.xpath("//h3[contains(text(),'Come vuoi accedere?')]");
             getWebDriverWait(30).withMessage("il titolo della pagina Accedi Area Riservata non è visibile").until(ExpectedConditions.visibilityOfElementLocated(titleBy));
-            getWebDriverWait(30).withMessage("Lo spid Button della pagina Accedi Area Riservata non è visibile").until(ExpectedConditions.elementToBeClickable(driver.findElement(By.id("spidButton"))));
+            getWebDriverWait(30).withMessage("Lo spid Button della pagina Accedi Area Riservata non è visibile").until(ExpectedConditions.elementToBeClickable(By.id("spidButton")));
             logger.info("Accedi Area Riservata Page caricata correttamente");
         } catch (TimeoutException e ){
             logger.error("Accedi Area Riservata Page non caricata correttamente con errore: "+e.getMessage());
