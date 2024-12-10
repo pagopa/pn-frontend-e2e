56d883879f5e85e9040ba8dc174bdd439b3a34b7
andreadefranco <andrea.defranco@netserv.it>
2024-12-10 17:58:48 +0100

    fix

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -234,8 +234,8 @@ public class PiattaformaNotifichePage extends BasePage {
 
     public void inserimentoCodiceIUN(String codiceIUN) {
         logger.info("Si inserisce il codice IUN...."  +codiceIUN);
-        webTool.waitTime(5);
-        getWebDriverWait(10).withMessage("Il campo per l'inserimento del codice IUN non è visibile").until(ExpectedConditions.visibilityOf(driver.findElement(By.id("iunMatch"))));
+        webTool.waitTime(10);
+        getWebDriverWait(100).withMessage("Il campo per l'inserimento del codice IUN non è visibile").until(ExpectedConditions.visibilityOf(driver.findElement(By.id("iunMatch"))));
         driver.findElement(By.id("iunMatch")).click();
         driver.findElement(By.id("iunMatch")).sendKeys(codiceIUN);
         logger.info("Codice IUN inserito");
