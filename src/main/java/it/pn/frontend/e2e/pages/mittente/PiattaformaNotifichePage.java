25bd1145089749511012ae2081ef5007159e5eda
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-10 17:29:03 +0100

    fix

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -1207,9 +1207,13 @@ public class PiattaformaNotifichePage extends BasePage {
     public void verificaNotificaCreata() {
         String notificationRequestId = "";
 
+        /**
         if (webDriverManager != null){
             netWorkInfos = webDriverManager.getNetWorkInfos();
         }
+         **/
+
+        netWorkInfos = WebDriverManager.getNetworkInfosThread().get();
 
         for (NetWorkInfo netWorkInfo : netWorkInfos) {
             if (netWorkInfo.getRequestUrl().contains("bff/v1/notifications/sent") && netWorkInfo.getRequestMethod().equals("POST")) {
