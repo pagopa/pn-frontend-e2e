e84d7692030cd1196559c1d9e73904bf93e8aac1
andreadefranco <andrea.defranco@netserv.it>
2024-12-12 09:36:08 +0100

    miglioria log

--- src/main/java/it/pn/frontend/e2e/section/destinatario/personaGiuridica/DelegatiImpresaSection.java
+++ src/main/java/it/pn/frontend/e2e/section/destinatario/personaGiuridica/DelegatiImpresaSection.java
@@ -140,7 +140,7 @@ public class DelegatiImpresaSection extends BasePage {
                     getWebDriverWait(30).withMessage("bottone rifiuta delega non cliccabile").until(ExpectedConditions.elementToBeClickable(By.id("reject-delegation-button")));
                     driver.findElement(By.id("reject-delegation-button")).click();
                     //Rifiuta la Delega pop-up
-                    getWebDriverWait(30).withMessage("bottone rifiuta delega non cliccabile").until(ExpectedConditions.elementToBeClickable(By.id("dialog-action-button")));
+                    getWebDriverWait(30).withMessage("bottone rifiuta delega non cliccabile pop-up").until(ExpectedConditions.elementToBeClickable(By.id("dialog-action-button")));
                     driver.findElement(By.id("dialog-action-button")).click();
 
                 }
