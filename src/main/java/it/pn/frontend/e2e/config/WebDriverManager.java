bdac73755a234041e61e0a403ae4b2d15da531ff
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-12 19:37:49 +0100

    fix

--- src/main/java/it/pn/frontend/e2e/config/WebDriverManager.java
+++ src/main/java/it/pn/frontend/e2e/config/WebDriverManager.java
@@ -249,8 +249,7 @@ public class WebDriverManager {
     }
 
     public void clearNetWorkInfos() {
-        networkInfosThread.get().clear();
-        networkInfosThread.remove();
+        NetworkInfoManager.clearNetworkInfos();
     }
 
 
