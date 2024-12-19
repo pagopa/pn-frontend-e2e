925618312a6491ad7000dff2de5cfab00df6829f
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-19 16:09:11 +0100

    fix calendar

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -347,7 +347,7 @@ public class PiattaformaNotifichePage extends BasePage {
         // Step 3: Wait for the calendar pop-up to appear
         WebElement calendar1 = getWebDriverWait(10).until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".MuiDateCalendar-root"))));  // Adjust based on your app
 
-        getWebDriverWait(10).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//button[@title='Previous month']")));
+        getWebDriverWait(10).until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//button[@title='Previous month']"))));
         WebElement previousMonthAButton = driver.findElement(By.xpath("//div[contains(@class, 'MuiDateCalendar-root')]//button[@title='Previous month']"));
         int clickA = 0;
         WebElement monthAToSelect = null;
