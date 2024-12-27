4b010957cf54c9f571b8347ae511b9fcdca8e991
angelo.minisci <angelo.minisci@grupposcai.it>
2024-12-27 09:58:43 +0100

    fix

--- src/main/java/it/pn/frontend/e2e/pages/destinatario/personaFisica/NotifichePFPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/destinatario/personaFisica/NotifichePFPage.java
@@ -80,6 +80,8 @@ public class NotifichePFPage extends BasePage {
     }
 
     public void waitLoadNotificheDEPage() {
+        getWebDriverWait(60).withMessage("Notifiche DE Page non caricata correttamente: il titolo non è visibile").until(ExpectedConditions.visibilityOf(driver.findElement(By.id("Le tue notifiche-page"))));
+        getWebDriverWait(60).withMessage("Notifiche DE Page non caricata correttamente: la tabella delle notifiche non è visibile").until(ExpectedConditions.visibilityOf(driver.findElement(By.id("notifications-table"))));
         titleLabel = driver.findElement(By.id("Le tue notifiche-page"));
         tableNotifiche = driver.findElement(By.id("notifications-table"));
         getWebDriverWait(25).withMessage("Notifiche DE Page non caricata correttamente: il titolo non è visibile").until(ExpectedConditions.visibilityOf(driver.findElement(By.id("Le tue notifiche-page"))));
