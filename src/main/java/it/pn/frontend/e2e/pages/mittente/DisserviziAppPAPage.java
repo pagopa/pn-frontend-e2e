432eb5147bfb21ec972ba5bf4d9a854c523999dc
angelo.minisci <angelo.minisci@grupposcai.it>
2025-01-06 12:13:30 +0100

    prova fix

--- src/main/java/it/pn/frontend/e2e/pages/mittente/DisserviziAppPAPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/DisserviziAppPAPage.java
@@ -202,7 +202,7 @@ public class DisserviziAppPAPage extends BasePage {
     public void checkDisserviziDisponibili() {
         aggiornamentoPagina();
         webTool.waitTime(15);
-        List<WebElement> statusList = driver.findElements(By.xpath("//tr[@id='tableDowntimeLog.row']//td//div[@data-testid='downtime-status']"));
+        List<WebElement> statusList = elements(By.xpath("//tr[@id='tableDowntimeLog.row']//td//div[@data-testid='downtime-status']"));
         if (!statusList.isEmpty()) {
             for (WebElement status : statusList) {
                 if (status.getText().contains("Risolto")) {
