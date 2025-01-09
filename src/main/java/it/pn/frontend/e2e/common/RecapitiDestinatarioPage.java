232b95ba2864cc8b7b9d7da7e28641e7fe62e6f5
angelo.minisci <angelo.minisci@grupposcai.it>
2025-01-09 12:57:09 +0100

    add wait

--- src/main/java/it/pn/frontend/e2e/common/RecapitiDestinatarioPage.java
+++ src/main/java/it/pn/frontend/e2e/common/RecapitiDestinatarioPage.java
@@ -325,7 +325,7 @@ public class RecapitiDestinatarioPage extends BasePage {
     }
 
     public boolean verificaMailField() {
-        webTool.waitTime(5);
+        webTool.waitTime(10);
         inserimentoMailField = driver.findElement(By.id("default_email"));
         return inserimentoMailField.isDisplayed();
     }
