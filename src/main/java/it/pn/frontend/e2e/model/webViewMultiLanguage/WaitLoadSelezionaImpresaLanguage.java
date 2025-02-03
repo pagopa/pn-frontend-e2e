62d814cdfd179afd368b9836dfd38c58f4e8314a
andreadefranco <andrea.defranco@netserv.it>
2025-02-03 18:41:34 +0100

    QA-5267

---
+++ src/main/java/it/pn/frontend/e2e/model/webViewMultiLanguage/WaitLoadSelezionaImpresaLanguage.java
@@ -0,0 +1,13 @@
+package it.pn.frontend.e2e.model.webViewMultiLanguage;
+import lombok.Data;
+
+@Data
+public class WaitLoadSelezionaImpresaLanguage {
+
+    String waitLoadSelezionaImpresaPageIt;
+    String waitLoadSelezionaImpresaPageEn;
+    String waitLoadSelezionaImpresaPageFr;
+    String waitLoadSelezionaImpresaPageDe;
+    String waitLoadSelezionaImpresaPageSl;
+
+}
