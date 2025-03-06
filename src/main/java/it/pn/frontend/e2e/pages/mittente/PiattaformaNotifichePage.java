920aa7ff19eeff6f7df67bfe060328bc2af097ce
luigi.franchi <luigi.franchi@alten.it>
2025-03-06 12:01:06 +0100

    calendar in progress

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -440,9 +440,9 @@ public class PiattaformaNotifichePage extends BasePage {
 
         logger.info("DATA INIZIO FIELD: " + dataInizioField.getAttribute("value"));
 
-        getWebDriverWait(3).until(ExpectedConditions.attributeToBe(dataInizioField, "value", da));
+        getWebDriverWait(3).withMessage("value mostrato" + da).until(ExpectedConditions.attributeToBe(dataInizioField, "value", da));
 
-        dataFineField = getWebDriverWait(10).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='endDate']")));
+        dataFineField = getWebDriverWait(10).withMessage("endDate visibile").until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='endDate']")));
 
         String[] arraySplitDateA = a.split("/");
 
