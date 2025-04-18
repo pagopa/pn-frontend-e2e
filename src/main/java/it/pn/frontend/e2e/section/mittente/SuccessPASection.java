11ca31ad309595ab9d3d721e7e09670930f9fed2
andreadefranco <andrea.defranco@netserv.it>
2025-04-18 17:05:33 +0200

    fix inserito un aumento di time

--- src/main/java/it/pn/frontend/e2e/section/mittente/SuccessPASection.java
+++ src/main/java/it/pn/frontend/e2e/section/mittente/SuccessPASection.java
@@ -49,7 +49,8 @@ public class SuccessPASection extends BasePage {
         logger.info("click pulsante vai alle notifiche");
 //        successButton = driver.findElement(By.id("go-to-notifications"));
 //        successButton.click();
-        WebElement vaiAlleNotifiche = getWebDriverWait(40)
+        webTool.waitTime(1);
+        WebElement vaiAlleNotifiche = getWebDriverWait(60)
                 .withMessage("Il pulsante con id 'go-to-notifications' non è visibile o cliccabile.")
                 .until(ExpectedConditions.elementToBeClickable(By.id("go-to-notifications")));
 
