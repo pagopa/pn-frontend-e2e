f96b132ab4e550fcd41fae18db0f76c2989b41e1
andreadefranco <andrea.defranco@netserv.it>
2025-05-19 12:38:14 +0200

    api key per dev

--- src/main/java/it/pn/frontend/e2e/rest/RestNotification.java
+++ src/main/java/it/pn/frontend/e2e/rest/RestNotification.java
@@ -95,7 +95,9 @@ private WebDriverConfig webDriverConfig;
         switch (env) {
             case "dev" -> {
                 httpClient2.setBaseUrlApi("https://api.dev.notifichedigitali.it");
-                httpClient2.setApiKey("6a735afb-5ef8-4576-b05d-5800003d53ba");
+//                httpClient2.setApiKey("6a735afb-5ef8-4576-b05d-5800003d53ba");
+                //TODO da provare
+                httpClient2.setApiKey("a9f0508d-c344-4347-807f-343bc8210996");
             }
             case "test" ->
                     httpClient2.setBaseUrlApi("https://api.test.notifichedigitali.it");
