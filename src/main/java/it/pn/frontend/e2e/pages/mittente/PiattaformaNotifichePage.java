5d8b9ff4130bcb7df2f957c1a76c49bcbaf3942d
andreadefranco <andrea.defranco@netserv.it>
2025-07-01 10:15:31 +0200

    eliminato commenti

--- src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/PiattaformaNotifichePage.java
@@ -1415,7 +1415,6 @@ public class PiattaformaNotifichePage extends BasePage {
         boolean testSuccess = false;
         for (int i = 0; i < 20; i++) {
             try {
-//                WebElement chipStatus = driver.findElement(By.id(statoNotifica + "-status"));
                 WebElement chipStatus = getWebDriverWait(5)
                         .until(ExpectedConditions.visibilityOfElementLocated(By.id(statoNotifica + "-status")));
                 if (chipStatus.isDisplayed()) {
@@ -1424,7 +1423,6 @@ public class PiattaformaNotifichePage extends BasePage {
                     testSuccess = true;
                     break;
                 }
-//            } catch (NoSuchElementException e) {
             } catch (TimeoutException | NoSuchElementException  e) {
                 logger.info("Dopo " + i + " tentativi la notifica non è ancora passata allo stato: " + statoNotifica);
             }
