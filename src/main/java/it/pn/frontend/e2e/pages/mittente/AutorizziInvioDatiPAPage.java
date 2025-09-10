cac1350ea3923f653b853d7da964236f8b5fa418
andreadefranco <andrea.defranco@netserv.it>
2025-09-10 12:21:16 +0200

    refactor driver.findElement in DashboardPage,AutorizziInvioDatiPAPage

--- src/main/java/it/pn/frontend/e2e/pages/mittente/AutorizziInvioDatiPAPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/mittente/AutorizziInvioDatiPAPage.java
@@ -1,8 +1,9 @@
 package it.pn.frontend.e2e.pages.mittente;
 
 import it.pn.frontend.e2e.common.BasePage;
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
@@ -20,24 +21,41 @@ public class AutorizziInvioDatiPAPage extends BasePage {
         this.driver = driver;
     }
 
+    //    public void waitLoadAutorizziInvioDatiPAPage() {
+//        try {
+//            WebElement titlePage = driver.findElement(By.cssSelector("h1.u-text-r-xl.u-margin-bottom-l"));
+//            getWebDriverWait(30).until(ExpectedConditions.visibilityOf(titlePage));
+//            logger.info("Autorizzi Invio Dati PA Page caricata");
+//        } catch (TimeoutException | NoSuchElementException e) {
+//            Assertions.fail("Il titolo di Autorizzi Invio Dati PA Page non è caricato con errore: " + e.getMessage());
+//        }
+//    }
     public void waitLoadAutorizziInvioDatiPAPage() {
-        try {
-            WebElement titlePage = driver.findElement(By.cssSelector("h1.u-text-r-xl.u-margin-bottom-l"));
-            getWebDriverWait(30).until(ExpectedConditions.visibilityOf(titlePage));
-            logger.info("Autorizzi Invio Dati PA Page caricata");
-        } catch (TimeoutException | NoSuchElementException e) {
-            Assertions.fail("Il titolo di Autorizzi Invio Dati PA Page non è caricato con errore: " + e.getMessage());
-        }
+        getWebDriverWait(30)
+                .withMessage("Titolo 'Autorizzi Invio Dati PA' non visualizzato")
+                .until(ExpectedConditions.visibilityOfElementLocated(
+                        By.cssSelector("h1.u-text-r-xl.u-margin-bottom-l")));
+
+        logger.info("Autorizzi Invio Dati PA Page caricata");
     }
 
+    //    public void selezionareInvia() {
+//        logger.info("Click button Invia");
+//        try {
+//            inviaButton = driver.findElement(By.cssSelector("button[name='confirm']"));
+//            getWebDriverWait(30).until(ExpectedConditions.elementToBeClickable(inviaButton));
+//            inviaButton.click();
+//        } catch (TimeoutException e) {
+//            Assertions.fail("Il bottone Invia nella pagina Autorizza Invio Dati non è stato cliccato con errore: " + e.getMessage());
+//        }
+//    }
     public void selezionareInvia() {
         logger.info("Click button Invia");
-        try {
-            inviaButton = driver.findElement(By.cssSelector("button[name='confirm']"));
-            getWebDriverWait(30).until(ExpectedConditions.elementToBeClickable(inviaButton));
-            inviaButton.click();
-        } catch (TimeoutException e) {
-            Assertions.fail("Il bottone Invia nella pagina Autorizza Invio Dati non è stato cliccato con errore: " + e.getMessage());
-        }
+
+        getWebDriverWait(35)
+                .withMessage("Bottone Invia non cliccabile")
+                .until(ExpectedConditions.elementToBeClickable(
+                        By.cssSelector("button[name='confirm']")))
+                .click();
     }
 }
