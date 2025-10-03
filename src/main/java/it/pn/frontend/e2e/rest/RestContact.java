52f70b18e75327bfafed4727b5b6b2a6a971d0e7
luigi.franchi <luigi.franchi@alten.it>
2025-10-03 11:28:06 +0200

    Debug per controllo su risposte lista indirizzi in parallelismo

--- src/main/java/it/pn/frontend/e2e/rest/RestContact.java
+++ src/main/java/it/pn/frontend/e2e/rest/RestContact.java
@@ -51,6 +51,10 @@ public class RestContact {
     @Getter
     private String sessionToken;
 
+    @Setter
+    @Getter
+    private String scenarioId;
+
     @Autowired
     public RestContact(WebDriverConfig webDriverConfig, CustomHttpClient<?, String> httpClient) {
         this.httpClient = httpClient;
@@ -183,9 +187,10 @@ public class RestContact {
 
         try {
             headers.put("Authorization", setAuthorizationToken());
+            headers.put("Scenario", scenarioId);
             List<DigitalAddress> response = httpClientDigitalAddress.sendHttpGetRequestListDigitalAddress(url, headers, DigitalAddress.class);
-            logger.info("Risposta ricevuta: {}", response);
-            logger.info("Indirizzi digitali ricevuti con successo");
+            logger.info("Risposta ricevuta per {}: {}", scenarioId, response);
+            logger.info("Indirizzi digitali per {} ricevuti con successo", scenarioId);
             return response;
         } catch (IOException e) {
             logger.error("Error during getAllDigitalAddress {}", e.getMessage());
--- src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaFisica/LoginPersonaFisicaPagoPA.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/destinatario/personaFisica/LoginPersonaFisicaPagoPA.java
@@ -13,6 +13,7 @@ import it.pn.frontend.e2e.config.CustomHttpClient;
 import it.pn.frontend.e2e.config.DataPopulationConfig;
 import it.pn.frontend.e2e.config.WebDriverConfig;
 import it.pn.frontend.e2e.config.WebDriverManager;
+import it.pn.frontend.e2e.listeners.HooksNew;
 import it.pn.frontend.e2e.listeners.NetWorkInfo;
 import it.pn.frontend.e2e.pages.destinatario.personaFisica.*;
 import it.pn.frontend.e2e.pages.mittente.PiattaformaNotifichePage;
@@ -94,6 +95,9 @@ public class LoginPersonaFisicaPagoPA extends BasePage{
 
     @Autowired
     private RestContact restContact;
+
+    @Autowired
+    private HooksNew hooksNew;
 
     @PostConstruct
     public void init(){
@@ -850,6 +854,7 @@ public class LoginPersonaFisicaPagoPA extends BasePage{
     @And("Rimuovi da API tutti i recapiti per persona fisica se esistono")
     public void clearRecapitiPF() {
         restContact.setSessionToken(sessionToken);
+        restContact.setScenarioId(hooksNew.getScenario());
         var digitalAddresses = restContact.getAllDigitalAddress();
         if (digitalAddresses != null && !digitalAddresses.isEmpty()) {
             digitalAddresses.forEach(address -> {
