c20b077dd87c79fc4d2aa37482e84d0cd93798e6
luigi.franchi <luigi.franchi@alten.it>
2025-10-06 18:44:13 +0200

    Risoluzione conflitti con develop

--- src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/NotificaMittentePagoPATest.java
+++ src/test/java/it/pn/frontend/e2e/stepDefinitions/mittente/NotificaMittentePagoPATest.java
@@ -2498,36 +2498,6 @@ public class NotificaMittentePagoPATest extends BasePage {
         piattaformaNotifichePage.verificaEsistenzaPaginaNonTrovata();
     }
 
-
-
-    @And("Nel portale Send {string} accedere ad una rotta non esistente")
-    public void nelPortaleSendAccedereAdUnaRottaNonEsistente(String portal) {
-        portal = portal.toLowerCase();
-        String env = this.webDriverConfig.getEnvironment();
-        switch (portal) {
-            case PF:
-                driver.get(webDriverConfig.getBaseUrlPfTest()+"prova");
-//                this.driver.get("https://cittadini." + env + ".notifichedigitali.it/prova");
-                break;
-            case PG:
-                driver.get(webDriverConfig.getBaseUrlPgTest()+"prova");
-//                this.driver.get("https://imprese." + env + ".notifichedigitali.it/prova");
-                break;
-            case PA:
-                driver.get(webDriverConfig.getUrlMittente()+"/prova");
-//                this.driver.get("https://selfcare." + env + ".notifichedigitali.it/prova");
-                break;
-            default:
-                Assertions.fail("Tipologia di portale non specificato o errato!");
-        }
-    }
-
-    @And("Verifica esistenza Pagina non trovata")
-    public void verificaEsistenzaPaginaNonTrovata() {
-        piattaformaNotifichePage.verificaEsistenzaPaginaNonTrovata();
-    }
-
-
     @And("Click Torna alla home")
     public void clickTornaAllaHome() {
         piattaformaNotifichePage.clickTornaAllaHome();
