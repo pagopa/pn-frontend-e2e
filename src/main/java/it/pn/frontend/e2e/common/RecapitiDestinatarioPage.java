3ef12e64e2e4d99feb8632fa88cb634a528c2ae1
luigi.franchi <luigi.franchi@alten.it>
2025-10-23 15:59:08 +0200

    Fix su copy flusso attivazione DD e banner DD non presente

--- src/main/java/it/pn/frontend/e2e/common/RecapitiDestinatarioPage.java
+++ src/main/java/it/pn/frontend/e2e/common/RecapitiDestinatarioPage.java
@@ -2039,9 +2039,8 @@ public class RecapitiDestinatarioPage extends BasePage {
         getWebDriverWait(10)
                 .withMessage("Testo nella modale Importanza aggiunta contatti non corretto o non presente")
                 .until(ExpectedConditions.and(
-                        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//p[contains(text(), 'Per attivare il domicilio digitale SEND abbiamo bisogno del tuo indirizzo email, così possiamo')]"))),
-                        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//strong[contains(text(), 'una comunicazione a valore legale per te.')]"))),
-                        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//p[contains(text(), 'Ricorda che se non leggi in tempo una notifica SEND, potresti non essere al corrente di eventuali scadenze e incorrere in sanzioni.')]"))))
+                        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains(text(),'Per attivare il domicilio digitale SEND abbiamo bisogno del tuo indirizzo email, così possiamo')]"))),
+                        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//strong[contains(text(), 'una comunicazione a valore legale per te.')]"))))
                 );
     }
 
@@ -2050,9 +2049,9 @@ public class RecapitiDestinatarioPage extends BasePage {
             verificaPresenza("Il banner di domicilio digitale non attivato non è presente", ExpectedConditions.visibilityOfElementLocated(
                     By.xpath("//div[@data-testid='addDomicileBanner' and contains(@class,'MuiAlert-outlinedInfo')]")));
             verificaPresenza("Il banner di domicilio digitale non attivato non ha il testo corretto", ExpectedConditions.visibilityOfElementLocated(
-                    By.xpath("//div[@data-testid='addDomicileBanner' and contains(@class,'MuiAlert-outlinedInfo')]//p[contains(text(), 'Niente più documenti cartacei: attiva SEND come Domicilio Digitale')]")));
+                    By.xpath("//div[@data-testid='addDomicileBanner' and contains(@class,'MuiAlert-outlinedInfo')]//p[contains(text(), 'Niente più documenti cartacei: attiva SEND come domicilio digitale')]")));
             verificaPresenza("Il banner di domicilio digitale non attivato non ha il sottotesto corretto", ExpectedConditions.visibilityOfElementLocated(
-                    By.xpath("//div[@data-testid='addDomicileBanner' and contains(@class,'MuiAlert-outlinedInfo')]//p[contains(text(), 'Scegli la piattaforma SEND come Domicilio Digitale per ricevere le prossime notifiche solo in digitale e leggerle in tempo reale, ovunque tu sia.')]")));
+                    By.xpath("//div[@data-testid='addDomicileBanner' and contains(@class,'MuiAlert-outlinedInfo')]//p[contains(text(), 'Scegli la piattaforma SEND come domicilio digitale per ricevere le prossime notifiche solo in digitale e leggerle in tempo reale, ovunque tu sia.')]")));
         } catch (TimeoutException e) {
             Assertions.fail("Il banner di domicilio digitale non attivato non è presente");
         }
@@ -2123,7 +2122,7 @@ public class RecapitiDestinatarioPage extends BasePage {
         getWebDriverWait(10)
                 .withMessage("Testo nella TYP Hai attivato il tuo domicilio digitale su SEND non corretto o non presente")
                 .until(ExpectedConditions.and(
-                        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@data-testid='wizard-feedback-step']//p[contains(text(), 'Riceverai le tue comunicazioni a valore legale al tuo Domicilio digitale su SEND e un messaggio ai recapiti che hai scelto.')]"))),
+                        ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[@data-testid='wizard-feedback-step']//p[contains(text(), 'Riceverai le tue comunicazioni a valore legale al tuo domicilio digitale su SEND e un messaggio ai recapiti che hai scelto.')]"))),
                         ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//button[@data-testid='wizard-feedback-button' and contains(text(),'Ok, ho capito')]")))
                         )
                 );
