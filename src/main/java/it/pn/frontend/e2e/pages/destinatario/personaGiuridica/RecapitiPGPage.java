122e56b64d87d5cedb1c0e5c37b44c0992fa10a5
luigi.franchi <luigi.franchi@alten.it>
2025-10-24 15:54:30 +0200

    Fix copy su TYP

--- src/main/java/it/pn/frontend/e2e/pages/destinatario/personaGiuridica/RecapitiPGPage.java
+++ src/main/java/it/pn/frontend/e2e/pages/destinatario/personaGiuridica/RecapitiPGPage.java
@@ -103,7 +103,7 @@ public class RecapitiPGPage extends BasePage {
         getWebDriverWait(10)
                 .withMessage("Testo nella TYP Hai attivato il domicilio digitale su SEND per PG non corretto o non presente")
                 .until(ExpectedConditions.and(
-                                ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@data-testid='wizard-feedback-step']//p[contains(text(), 'impresa riceverà le comunicazioni a valore legale al Domicilio digitale su SEND e un messaggio ai recapiti che hai scelto.')]"))),
+                                ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@data-testid='wizard-feedback-step']//p[contains(text(), 'impresa riceverà le comunicazioni a valore legale al domicilio digitale su SEND e un messaggio ai recapiti che hai scelto.')]"))),
                                 ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//button[@data-testid='wizard-feedback-button' and contains(text(),'Ok, ho capito')]")))
                         )
                 );
@@ -141,13 +141,13 @@ public class RecapitiPGPage extends BasePage {
     public void verificaDellaTYPHaiTrasferitoIlDomicilioDigitaleSuSEND() {
         getWebDriverWait(10)
                 .withMessage("Titolo nella TYP Hai trasferito il domicilio digitale su SEND per PG non corretto o non presente")
-                .until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@data-testid='wizard-feedback-step']//h4[contains(text(), 'Hai trasferito il domicilio digitale su SEND')]")))
+                .until(ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@data-testid='wizard-feedback-step']//h4[contains(text(), 'Hai trasferito il domicilio digitale della tua impresa su SEND')]")))
                 );
 
         getWebDriverWait(10)
                 .withMessage("Testo nella TYP Hai trasferito il domicilio digitale su SEND per PG non corretto o non presente")
                 .until(ExpectedConditions.and(
-                                ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@data-testid='wizard-feedback-step']//p[contains(text(), 'impresa riceverà le comunicazioni a valore legale al Domicilio digitale su SEND e un messaggio ai recapiti che hai scelto.')]"))),
+                                ExpectedConditions.visibilityOfElementLocated((By.xpath("//div[@data-testid='wizard-feedback-step']//p[contains(text(), 'impresa riceverà le comunicazioni a valore legale al domicilio digitale su SEND e un messaggio ai recapiti che hai scelto.')]"))),
                                 ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//button[@data-testid='wizard-feedback-button' and contains(text(),'Ok, ho capito')]")))
                         )
                 );
