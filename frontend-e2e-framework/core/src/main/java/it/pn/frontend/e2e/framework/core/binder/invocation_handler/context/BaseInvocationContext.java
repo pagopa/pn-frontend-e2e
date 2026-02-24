354748b32cdbcbe4df82d49697ffce3ead88ecbb
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-24 20:43:25 +0100

    refactor(core/BaseInvocationContext): abstract -> public

--- frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/binder/invocation_handler/context/BaseInvocationContext.java
+++ frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/binder/invocation_handler/context/BaseInvocationContext.java
@@ -1,14 +1,12 @@
 package it.pn.frontend.e2e.framework.core.binder.invocation_handler.context;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.lang.reflect.Method;
 
 @Getter
-public abstract class BaseInvocationContext {
+@RequiredArgsConstructor
+public class BaseInvocationContext {
     private final Method method;
-
-    protected BaseInvocationContext(Method method) {
-        this.method = method;
-    }
 }
