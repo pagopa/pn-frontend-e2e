df595128953e8d45b2e6c3996af58976a4e634e1
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-24 22:15:12 +0100

    refactor(core/PresentationElement): add base information

--- frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/adapter/model/PresentationElement.java
+++ frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/adapter/model/PresentationElement.java
@@ -1,4 +1,26 @@
 package it.pn.frontend.e2e.framework.core.adapter.model;
 
+import it.pn.frontend.e2e.framework.core.model.locator.Locator;
+import it.pn.frontend.e2e.framework.core.model.locator.LocatorType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PresentationElement {
+
+    private Locator<? extends LocatorType> locator;
+    private String elementType;
+    private String name;
+    private boolean displayed;
+    private boolean enabled;
+    private String text;
+
+    public PresentationElement(Locator<? extends LocatorType> locator, String elementType, String name) {
+        this.locator = locator;
+        this.elementType = elementType;
+        this.name = name;
+        this.displayed = false;
+        this.enabled = false;
+    }
 }
