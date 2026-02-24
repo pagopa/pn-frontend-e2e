d7e109b99125bbf07abd3d9be4f836ae70af4a8d
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-24 20:13:04 +0100

    refactor(core/BaseInvocationContext): remove unused dependencies

--- frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/binder/invocation_handler/context/BaseInvocationContext.java
+++ frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/binder/invocation_handler/context/BaseInvocationContext.java
@@ -1,23 +1,14 @@
 package it.pn.frontend.e2e.framework.core.binder.invocation_handler.context;
 
-import it.pn.frontend.e2e.framework.core.adapter.IPresentationApiAdapter;
 import lombok.Getter;
 
 import java.lang.reflect.Method;
 
 @Getter
 public abstract class BaseInvocationContext {
-    private final Object proxy;
     private final Method method;
-    private final Object[] args;
-    private final IPresentationApiAdapter adapter;
-    private final Class<?> boundType;
 
-    protected BaseInvocationContext(Object proxy, Method method, Object[] args, IPresentationApiAdapter adapter, Class<?> boundType) {
-        this.proxy = proxy;
+    protected BaseInvocationContext(Method method) {
         this.method = method;
-        this.args = args;
-        this.adapter = adapter;
-        this.boundType = boundType;
     }
 }
