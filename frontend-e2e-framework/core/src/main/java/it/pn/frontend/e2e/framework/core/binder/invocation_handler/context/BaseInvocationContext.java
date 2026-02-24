3d9ffce0c76866db53942c501af1e050b75658ff
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-24 20:09:00 +0100

    refactor(core/BaseInvocationContext): use @Getter

--- frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/binder/invocation_handler/context/BaseInvocationContext.java
+++ frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/binder/invocation_handler/context/BaseInvocationContext.java
@@ -1,8 +1,11 @@
 package it.pn.frontend.e2e.framework.core.binder.invocation_handler.context;
 
 import it.pn.frontend.e2e.framework.core.adapter.IPresentationApiAdapter;
+import lombok.Getter;
+
 import java.lang.reflect.Method;
 
+@Getter
 public abstract class BaseInvocationContext {
     private final Object proxy;
     private final Method method;
@@ -16,25 +19,5 @@ public abstract class BaseInvocationContext {
         this.args = args;
         this.adapter = adapter;
         this.boundType = boundType;
-    }
-
-    public IPresentationApiAdapter getAdapter() {
-        return adapter;
-    }
-
-    public Object[] getArgs() {
-        return args;
-    }
-
-    public Class<?> getBoundType() {
-        return boundType;
-    }
-
-    public Method getMethod() {
-        return method;
-    }
-
-    public Object getProxy() {
-        return proxy;
     }
 }
