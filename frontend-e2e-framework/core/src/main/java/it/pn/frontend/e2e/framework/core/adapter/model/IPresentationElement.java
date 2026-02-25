82aa03a1e8a864a1ab05693120562b666fa5ef1b
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-25 14:02:23 +0100

    feat(core/IPresentationElement): add PresentationElement contract

---
+++ frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/adapter/model/IPresentationElement.java
@@ -0,0 +1,5 @@
+package it.pn.frontend.e2e.framework.core.adapter.model;
+
+public interface IPresentationElement {
+
+}
--- frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/adapter/model/PresentationElement.java
+++ frontend-e2e-framework/core/src/main/java/it/pn/frontend/e2e/framework/core/adapter/model/PresentationElement.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class PresentationElement {
+public class PresentationElement implements IPresentationElement{
 
     private Locator<? extends LocatorType> locator;
     private String elementType;
