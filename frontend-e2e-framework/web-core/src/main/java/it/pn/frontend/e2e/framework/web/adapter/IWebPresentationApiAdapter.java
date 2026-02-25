756eff75fe0973710f8f5e59198b8a8c41787919
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-25 11:52:02 +0100

    feat(web-core/IWebPresentationApiAdapter): add navigateTo

--- frontend-e2e-framework/web-core/src/main/java/it/pn/frontend/e2e/framework/web/adapter/IWebPresentationApiAdapter.java
+++ frontend-e2e-framework/web-core/src/main/java/it/pn/frontend/e2e/framework/web/adapter/IWebPresentationApiAdapter.java
@@ -3,4 +3,5 @@ package it.pn.frontend.e2e.framework.web.adapter;
 import it.pn.frontend.e2e.framework.core.adapter.IPresentationApiAdapter;
 
 public interface IWebPresentationApiAdapter extends IPresentationApiAdapter {
+    void navigateTo(String url);
 }
