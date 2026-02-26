dfd29171dae41242897caa3de0ff56ee8daeef84
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-26 21:23:51 +0100

    refactor(core/Capability): method get return Optional

--- frontend-e2e-framework/core/src/main/java/it/frontend/e2e/framework/core/capability/Capability.java
+++ frontend-e2e-framework/core/src/main/java/it/frontend/e2e/framework/core/capability/Capability.java
@@ -4,6 +4,8 @@ import it.frontend.e2e.framework.core.model.AbstractPresentationElement;
 import it.frontend.e2e.framework.core.model.Location;
 import it.frontend.e2e.framework.core.model.Selector;
 
+import java.util.Optional;
+
 public interface Capability<S extends Selector, L extends Location, E extends AbstractPresentationElement<S,L>> {
-    E get();
+    Optional<E> get();
 }
