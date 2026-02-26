f2e08ec2d76dc4dc42466ac38e0a81377bdc77f2
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-26 21:47:48 +0100

    test(core/): add TestElement, TestLocation, TestSelector

---
+++ frontend-e2e-framework/core/src/test/java/it/frontend/e2e/framework/core/model/TestElement.java
@@ -0,0 +1,7 @@
+package it.frontend.e2e.framework.core.model;
+
+public final class TestElement extends AbstractPresentationElement<TestSelector, TestLocation> {
+    public TestElement(TestSelector selector, TestLocation location) {
+        super(selector, location);
+    }
+}
