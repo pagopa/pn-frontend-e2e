9b5c65c5c6b547f7f8676e0e6ded9f8431a247b3
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-26 17:13:41 +0100

    chore: fix artifactId

--- src/test/java/it/pn/frontend/e2e/steps/send/mittenti/Dashboard.java
+++ src/test/java/it/pn/frontend/e2e/steps/send/mittenti/Dashboard.java
@@ -1,12 +1,12 @@
 package it.pn.frontend.e2e.steps.send.mittenti;
 
 import it.pn.frontend.e2e.framework.annotation.Selector;
-import it.pn.frontend.e2e.framework.core.capability.common.Readable;
 import it.pn.frontend.e2e.framework.web.annotation.PageInfo;
 import it.pn.frontend.e2e.framework.web.domain.Page;
 
 @PageInfo(url = "/dashboard")
 public interface Dashboard extends Page {
+
     @Selector(value = "#dashboard-header")
     Readable header();
 
