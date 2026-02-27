981b160d54d5cb117549be1dbcaddba2c009dc0c
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-02-27 02:13:44 +0100

    refactor(web-core/WebPresentationGateway): extends IPresentationGateway

--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/adapter/IWebPresentationApiAdapter.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/adapter/IWebPresentationApiAdapter.java
@@ -5,6 +5,6 @@ import it.frontend.e2e.framework.web.adapter.model.WebPresentationElement;
 import it.frontend.e2e.framework.web.adapter.model.locator.WebLocation;
 import it.frontend.e2e.framework.web.adapter.model.selector.WebSelector;
 
-public interface IWebPresentationApiAdapter extends IPresentationApiAdapter<WebPresentationElement, WebSelector, WebLocation> {
+public interface IWebPresentationApiAdapter extends IPresentationApiAdapter<WebSelector, WebLocation, WebPresentationElement> {
 
 }
