1a8f1fc21e7dd356fc4cea4b0cda384723483f52
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-03-05 13:07:24 +0100

    feat(web-core/): ReadableImpl.java implementation

--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/capability/handler/ReadableCapabilityHandler.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/capability/handler/ReadableCapabilityHandler.java
@@ -1,12 +1,13 @@
 package it.frontend.e2e.framework.web.capability.handler;
 
 import it.frontend.e2e.framework.core.capability.handler.AbstractCapabilityHandler;
+import it.frontend.e2e.framework.web.adapter.IWebPresentationApiAdapter;
 import it.frontend.e2e.framework.web.capability.core.Readable;
 import it.frontend.e2e.framework.web.capability.impl.ReadableImpl;
 
 public class ReadableCapabilityHandler extends AbstractCapabilityHandler<Readable> {
 
-    public ReadableCapabilityHandler() {
-        super(new ReadableImpl());
+    public ReadableCapabilityHandler(IWebPresentationApiAdapter adapter) {
+        super(new ReadableImpl(adapter));
     }
 }
--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/capability/impl/ReadableImpl.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/capability/impl/ReadableImpl.java
@@ -1,23 +1,46 @@
 package it.frontend.e2e.framework.web.capability.impl;
 
 import it.frontend.e2e.framework.core.assertion.AssertionAction;
+import it.frontend.e2e.framework.core.capability.context.CapabilityContext;
+import it.frontend.e2e.framework.web.adapter.IWebPresentationApiAdapter;
 import it.frontend.e2e.framework.web.capability.core.Readable;
 import it.frontend.e2e.framework.web.model.WebPresentationElement;
+import it.frontend.e2e.framework.web.model.WebSelector;
+import lombok.RequiredArgsConstructor;
 
-public class ReadableImpl<T> implements Readable<T> {
+@RequiredArgsConstructor
+public class ReadableImpl<T extends WebPresentationElement> implements Readable<T> {
+
+    private final IWebPresentationApiAdapter adapter;
 
     @Override
+    @SuppressWarnings("unchecked")
     public T read() {
-        return null;
+        WebSelector webSelector = WebSelector.of(CapabilityContext.selector());
+        return (T) adapter.findElement(webSelector).orElse(null);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public T readAndAssert() {
-        return null;
+        WebSelector webSelector = WebSelector.of(CapabilityContext.selector());
+        return (T) adapter.findElement(webSelector).orElseThrow(
+                () -> {
+                    String errorMsg = String.format(
+                            "Element not found for selector: %s. " +
+                            "Check if selector is correct or element is present in DOM. " +
+                            "Consider increasing wait timeout if page loads slowly.",
+                            webSelector
+                    );
+                    return new RuntimeException(errorMsg);
+                }
+        );
     }
 
     @Override
     public T readAndAssert(AssertionAction<WebPresentationElement> assertionAction) {
-        return null;
+        T element = readAndAssert();
+        assertionAction.assertOn(element);
+        return element;
     }
 }
