8a13740bf66ff0451c71728cc7e9a77a1394db48
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-03-05 13:06:37 +0100

    feat(web-core/Page): add assertLoaded

--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/domain/Page.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/domain/Page.java
@@ -1,4 +1,7 @@
 package it.frontend.e2e.framework.web.domain;
 
 public interface Page extends Component {
+    default void assertLoaded() {
+        throw new UnsupportedOperationException("Method assertLoaded() not implemented for " + this.getClass().getName());
+    }
 }
