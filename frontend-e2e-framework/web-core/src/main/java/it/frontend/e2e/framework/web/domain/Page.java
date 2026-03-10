9a233f1712792a30efdaf63611efb6b284c85a14
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-03-10 01:45:30 +0100

    refactor(web-core/Page): hierarchy

--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/domain/Page.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/domain/Page.java
@@ -1,6 +1,9 @@
 package it.frontend.e2e.framework.web.domain;
 
-public interface Page extends DomNode {
+import it.frontend.e2e.framework.core.capability.Capability;
+import it.frontend.e2e.framework.core.model.DomainElement;
+
+public interface Page extends DomainElement, Capability {
     default void assertLoaded() {
         throw new UnsupportedOperationException("Method assertLoaded() not implemented for " + this.getClass().getName());
     }
